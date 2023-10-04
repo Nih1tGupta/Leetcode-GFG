4e47ccb7e39b87c3efcdbe5bd09a1e1e2a59b7b4
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-10-04 20:15:51 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Power of Four/power-of-four.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int isPowerOfFour(unsigned int n)
+    {
+     if(n == 0) return 0;
+        while(n % 4 == 0) 
+            n /= 4;
+        return n == 1;
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+/*Driver program to test above function*/
+int main()
+{
+int t,n;
+cin>>t;
+while(t--)
+{
+cin>>n;
+Solution ob;
+if(ob.isPowerOfFour(n))
+	cout<<1<<endl;
+else
+	cout<<0<<endl;
+}
+//getchar();
+}
+
+// } Driver Code Ends
