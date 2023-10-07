45f731939399f0f3f7bdedff5d580006231b2e4b
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-10-07 19:47:15 +0530

    Time: 2 ms (50.23%), Space: 6.4 MB (37.04%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        int ans=1;
+   if(n==2){return 1;}
+   if(n==3){return 2;}
+   while(n>4){
+      ans=ans*3;
+      n-=3;
+   }
+   ans=ans*n;
+   return ans;
+    }
+};
