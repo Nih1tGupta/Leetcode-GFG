3e16b7b3d0ef327d2dad5fde255e9911815076ec
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-10-08 22:09:11 +0530

    Time: 63 ms (23.91%), Space: 13.8 MB (20.05%) - LeetHub

---
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+
+        int n = nums1.size();
+        int n1 = nums2.size();
+        vector<vector<int>> memo(n, vector<int>(n1, INT_MIN));
+        
+        function<int(int, int)> dp = [&](int i, int j) {
+            if (i == n || j == n1) {
+                return INT_MIN;
+            }
+            
+            if (memo[i][j] != INT_MIN) {
+                return memo[i][j];
+            }
+            
+            memo[i][j] = max(
+                nums1[i] * nums2[j] + max(dp(i + 1, j + 1), 0),
+                max(dp(i + 1, j), dp(i, j + 1))
+            );
+            
+            return memo[i][j];
+        };
+        
+        return dp(0, 0);
+  
+    }
+};
