a01be07d8b29d5d1173337440db198fa8a8fca12
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-07 01:56:22 +0530

    Time: 0 ms (100%), Space: 6.2 MB (64.45%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if( divisor==-1 && dividend==INT_MIN){return INT_MAX;}
-        return floor(dividend/divisor);
+        return (dividend/divisor);
    
     }
 };
