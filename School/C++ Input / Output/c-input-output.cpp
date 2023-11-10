91264dcfe30bc4aa706303cefd9e14a0bb928f47
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-10 19:22:18 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ Input / Output/c-input-output.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int multiplication(int A, int B){
+      int pro=A*B;
+      return  pro;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.multiplication(A,B) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
