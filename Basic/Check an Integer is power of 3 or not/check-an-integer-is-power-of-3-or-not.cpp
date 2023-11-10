da773fcc508e4b8105616adb81d2354d232b4ca2
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-10 13:54:10 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check an Integer is power of 3 or not/check-an-integer-is-power-of-3-or-not.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isPowerof3(int N){
+        for(int i = 0; i<32; i++){
+            if( pow(3,i)== N){
+                return "Yes";
+            }
+        }   return "No";
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isPowerof3(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
