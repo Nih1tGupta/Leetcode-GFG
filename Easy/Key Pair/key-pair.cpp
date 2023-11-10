cc07afbecb9f47127743138a4a7a10ace1aeb8fa
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-10 13:40:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Key Pair/key-pair.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	   sort(arr+0,arr+n);
+        int left = 0, right = n-1;
+        while(left < right)
+        {
+            int sum = arr[left] + arr[right];
+            if(sum == x)
+             return true;
+            else if(sum < x)
+                left++;
+            else
+             right--;
+        }
+        return false;
+    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
