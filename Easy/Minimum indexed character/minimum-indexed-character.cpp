c0e52f2ad958eef988cbfa5cfa8bb076c3568544
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-10 21:07:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum indexed character/minimum-indexed-character.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find the minimum indexed character.
+    int minIndexChar(string str, string patt)
+  {
+        for(int i=0;i<str.size();i++)
+            if(patt.find(str[i])!=string::npos) return i;
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        string patt;
+        cin>>str;
+        cin>>patt;
+        Solution obj;
+        cout<<obj.minIndexChar(str, patt)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
