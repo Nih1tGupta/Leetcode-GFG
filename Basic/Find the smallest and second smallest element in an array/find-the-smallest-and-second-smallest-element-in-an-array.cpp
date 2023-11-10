981bdd105fe457f2608e24cb4b8f320a3ce5b082
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-10 20:29:31 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find the smallest and second smallest element in an array/find-the-smallest-and-second-smallest-element-in-an-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> minAnd2ndMin(int a[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+
+        vector<int> ans = minAnd2ndMin(a, n);
+        if (ans[0] == -1)
+            cout << -1 << endl;
+        else 
+            cout << ans[0] << " " << ans[1] << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+vector<int> minAnd2ndMin(int a[], int n) {
+     vector<int> ans;
+    int small = INT_MAX;
+    int ssmall= INT_MAX;
+    for(int i = 0 ; i < n ; i++){
+        
+        if(a[i] < small){
+            ssmall = small;
+            small = a[i];
+        }
+        
+        else if(a[i] < ssmall and a[i] > small){
+            ssmall= a[i];
+        }
+    }
+    if(ssmall == INT_MAX){
+        ans.push_back(-1);
+        return ans;
+    }
+    ans.push_back(small);
+    ans.push_back(ssmall);
+    return ans;
+}
