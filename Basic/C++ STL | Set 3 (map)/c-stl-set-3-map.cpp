576706ba177f0b62c32f3ac843b50c5a3bfb3310
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-11 11:11:38 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/C++ STL | Set 3 (map)/c-stl-set-3-map.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+void add_value(map<int,int> &m,int x,int y);
+
+int find_value(map<int,int> &m,int x);
+void print_contents(map<int,int> &m);
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		map<int,int> m;
+		int q;
+		cin>>q;
+		while(q--)
+		{
+			char c;
+			cin>>c;
+			if(c=='a')
+			{
+				int x,y;
+				cin>>x>>y;
+				add_value(m,x,y);
+			}
+			if(c=='b')
+			{
+				int y;
+				cin>>y;
+				cout<<find_value(m,y)<<" ";
+			}
+			if(c=='c')
+			{
+				print_contents(m);
+			}
+		
+		
+		
+		}
+cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Adds a value with key x and value y to the map*/
+void add_value(map<int,int> &m,int x,int y){
+    m[x]=y;
+}
+
+int find_value(map<int,int> &m,int x)
+{
+    if(m.find(x)!=m.end()){
+        return m[x];
+    }
+    return -1;
+}
+
+void print_contents(map<int,int> &m)
+{
+    for(auto i:m){
+        cout<<i.first<<" "<<i.second<<" ";
+    }
+}
