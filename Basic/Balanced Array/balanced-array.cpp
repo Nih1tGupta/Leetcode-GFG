b2e2b6a1183889007f7c36e1a7bc157c846334fe
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-11 15:08:42 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Balanced Array/balanced-array.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int minValueToBalance(int a[], int n)
+    {int lsum=0,rsum=0;
+       for(int i=0;i<n/2;i++){
+           lsum+=a[i];
+       }
+       for(int i=n-1;i>=n/2;i--){
+           rsum+=a[i];
+       }
+       return abs(lsum-rsum);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;	
+		cout<<ob.minValueToBalance(a,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
