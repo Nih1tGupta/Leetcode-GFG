0d26a4f4a13829e3e4affe1fdfafa9e90fc01f1e
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-11 15:14:21 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum repeating number/maximum-repeating-number.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Returns maximum repeating element in arr[0..n-1].
+	// The array elements are in range from 0 to k-1
+	int maxRepeating(int *arr, int n, int k) {
+	       unordered_map<int,int>mp;int maxi=INT_MIN;int ans=0;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        for(int i=0;i<k;i++){
+            if(maxi<mp[i]){
+                maxi=mp[i];
+                ans=i;
+            }
+        }
+        return ans;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxRepeating(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
