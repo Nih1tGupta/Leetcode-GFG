4c1b0dc4565a290e7681da7c92bc76c7f06def7b
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-13 01:26:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Finding Position/finding-position.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthPosition(long long int n){
+     long long m=1;
+       while(n>1){
+            m=m*2;
+            n=n/2;
+       }
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout<<ob.nthPosition(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
