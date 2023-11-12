2bce287fa5014c8a14189193db97686099d0a166
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-12 12:04:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Crazy String/crazy-string.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string getCrazy (string s);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << getCrazy (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+string getCrazy (string S){
+   for(int i=1; i<S.length(); i++){
+        if(islower(S[0])){
+            if(i%2==0){S[i] = tolower(S[i]);}
+            else{S[i] = toupper(S[i]);}
+        }
+        else{
+            if(i%2==0){S[i] = toupper(S[i]);}
+            else{S[i] = tolower(S[i]);}
+        }
+    }
+    return S;
+}
