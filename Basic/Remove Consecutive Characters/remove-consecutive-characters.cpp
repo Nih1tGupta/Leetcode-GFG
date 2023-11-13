7feef6a51c9ef2acab278423c92902497ef31ed8
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-13 22:49:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Remove Consecutive Characters/remove-consecutive-characters.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+       string m="";
+        for(int i=0;i<S.length();i++){
+            if(S[i]!=S[i+1]){
+                m+=S[i];
+            }
+        }
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
