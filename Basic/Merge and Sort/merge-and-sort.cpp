948a85b21b2a60ad401948b56f93b7ff06e9e375
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-13 22:44:44 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Merge and Sort/merge-and-sort.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+    // Write your code here
+
+class Solution{
+    public:
+    int mergeNsort(int a[], int b[], int n, int m, int answer[])
+    {
+          set<int> ans;
+        for(int i = 0; i < n; i++)
+            ans.insert(a[i]);
+        for(int i = 0; i < m; i++)
+            ans.insert(b[i]);
+        int i = 0;
+        for(auto it : ans)
+            answer[i++] = it;
+        return ans.size();
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n;
+        int a[n+5];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        cin>>m;
+        int b[m+5];
+        for(int j=0;j<m;j++)
+            cin>>b[j];
+        Solution ob;
+        int answer[n+m+5];
+        int x=ob.mergeNsort(a, b, n, m, answer);
+        
+        for(int i=0;i<x;i++)
+        cout<<answer[i]<<" ";
+        cout<<endl;
+    }
+	
+	return 0;
+}
+// } Driver Code Ends
