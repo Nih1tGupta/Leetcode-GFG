13724837765fef74d30d89da6cd82b48b253a4bc
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-11-17 20:30:17 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Elements in the Range/elements-in-the-range.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+  {
+        sort(arr,arr+n);
+        int count = 0, k = A;
+        for(int i=0; i<n; i++) {
+            if(arr[i] == k and k<=B)
+            {    k++;   count++; } }
+        if(count == B-A+1) {
+            return 1; }
+        return 0;
+    }
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
