657f0b71582d01084f79cc7a8e9a71808f7f728b
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-12-03 18:31:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Change the string/change-the-string.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            Solution ob = new Solution();
+            String ans = ob.modify(s);
+            System.out.println(ans);
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    String modify(String s){
+        if(Character.isUpperCase(s.charAt(0)))
+        return s.toUpperCase();
+        return s.toLowerCase();
+    }
+}
