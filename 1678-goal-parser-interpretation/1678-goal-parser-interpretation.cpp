ec1c23c6021a963a1b9af11d890b6a302bfc3c3f
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-12-04 14:22:06 +0530

    Time: 3 ms (44.87%), Space: 6.5 MB (94.77%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string str="";
+        for(int i=0;i<command.size();i++){
+            if(command[i]=='G'){str.push_back('G');}
+            else if(command[i]=='('){
+                if(command[i+1]==')'){str.push_back('o');}
+                else{
+                    str.push_back('a');str.push_back('l');
+                }
+            }
+        }
+        return str;
+    }
+};
