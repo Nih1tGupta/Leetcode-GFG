c3b71df803f2c4087240e027043843bb46d2a833
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-12-16 13:38:43 +0530

    Time: 7 ms (76.44%), Space: 7.7 MB (79.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){return true;}
-        return false;
+      int freqs[26]={0};      
+      int freqt[26]={0};
+        for(auto it: s){freqs[it-'a']++;} 
+        for(auto it: t){freqt[it-'a']++;}
+        for(int i=0;i<26;i++){
+            if(freqs[i]!=freqt[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
