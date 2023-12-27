003527ef4e947aefc7f5fd2985599eff6ba6f506
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-12-27 17:12:35 +0530

    Time: 5 ms (15.19%), Space: 6.4 MB (54.26%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res=0;
+        for(int i=0;i<columnTitle.length();i++){
+            res*=26;
+            res+=( (columnTitle[i]-'A') + 1);
+           
+        }
+         return res;
+    }
+};
