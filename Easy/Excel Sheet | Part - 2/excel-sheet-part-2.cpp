6612446ebb164defdc2a5583465037e493fcd982
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2023-12-27 22:36:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Excel Sheet | Part - 2/excel-sheet-part-2.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    int ExcelColumnNumber(string s)
+    {
+       int res=0;int ans=0;
+       for(int i=0;i<s.length();i++){
+           res*=26;
+           res+=s[i]-'A'+1;
+       }
+       return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.ExcelColumnNumber(s)<<endl;
+    }
+}
+// } Driver Code Ends
