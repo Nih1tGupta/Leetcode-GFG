8fd76435f06d9208df75e5d68c1bcd70eaaf38c5
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-01-07 22:58:09 +0530

    Updated Solution - GfG to GitHub

--- Easy/Finding middle element in a linked list/finding-middle-element-in-a-linked-list.cpp
+++ Easy/Finding middle element in a linked list/finding-middle-element-in-a-linked-list.cpp
@@ -41,16 +41,15 @@ class Solution{
     /* Should return data of middle node. If linked list is empty, then  -1*/
     int getMiddle(Node *head)
     {
-    
-       Node*slow=head;Node*fast=head->next;
-       while(fast!=NULL){
-           fast=fast->next;
-           if(fast!=NULL){
-               fast=fast->next;
-           }
-           slow=slow->next;
-       }
-       return slow->data;
+        Node*slow=head;
+        Node*fast=head->next;
+        
+        while(fast !=NULL){
+            fast=fast->next;
+            if(fast!=NULL){fast=fast->next;}
+            slow=slow->next;
+        }
+        return slow->data;
     }
 };
 
