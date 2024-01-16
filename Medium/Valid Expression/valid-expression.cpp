d7320e763351a792ea1e8091658a1ad9f870f639
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-01-16 22:29:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Valid Expression/valid-expression.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+bool valid(string str);
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore();
+    while(t--)
+    {
+        string str;
+        getline(cin,str);
+        cout<<valid(str)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+bool valid(string s)
+{
+    stack<char>st;
+    for(int i=0;i<s.length();i++){
+        char ch= s[i];
+        if(ch=='('|| ch =='{'|| ch =='['){st.push(ch);}
+        else{
+            if(!st.empty()){
+                char top = st.top();
+                if(  (ch==')' and top =='(')  ||  (ch=='}' and top=='{')  || (ch==']' and top=='[')  ){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        
+    }
+    if(st.empty()){return true;}
+    return false;
+}
