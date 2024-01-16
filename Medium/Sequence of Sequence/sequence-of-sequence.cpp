52ab7b1693b344fd4432a9cd9fc93f45f07458ab
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-01-16 19:51:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sequence of Sequence/sequence-of-sequence.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int numberSequence(int m, int n){
+       
+        if (n==0){
+            return 1;
+        }
+        if(2*m<n){
+            return 0;
+        }
+        return numberSequence(m/2,n-1)+numberSequence(m-1,n);
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m, n;
+        cin>>m>>n;
+        
+        Solution ob;
+        cout<<ob.numberSequence(m, n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
