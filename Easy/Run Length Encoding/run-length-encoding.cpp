61c6382607a9a8d87299a3ef10046fce949bb3ee
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-02-11 22:59:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Run Length Encoding/run-length-encoding.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this function */
+
+string encode(string src)
+{     
+  string ans=""; int count=1;
+  for(int i=0;i<src.length();i++){
+     
+    if(src[i]==src[i+1]){count++;}
+    else{
+  
+   ans+=src[i] +to_string(count);
+     count=1;
+  }
+  }
+  return ans;
+}     
+ 
