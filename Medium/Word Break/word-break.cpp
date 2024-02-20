26c03686b62dbca57d04336cfdbb7407117b1076
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-02-20 19:57:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Word Break/word-break.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+// s : given string to search
+// dictionary : vector of available strings
+
+class Solution
+{
+public:
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+         if(s=="") return 1;
+        for(auto it : dictionary)
+            if(it==s.substr(0,it.size()))
+                if(wordBreak(n,s.substr(it.size()),dictionary)) return 1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dictionary;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dictionary.push_back(S);
+        }
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.wordBreak(n, s, dictionary)<<"\n";
+    }
+}
+
+// } Driver Code Ends
