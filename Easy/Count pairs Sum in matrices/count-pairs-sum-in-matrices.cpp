de2f68e586f01fad48f49bd46ac887a040e04fc2
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-03-11 16:49:13 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -1,4 +1,5 @@
 //{ Driver Code Starts
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,7 +12,7 @@ public:
 	
 	int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int n, int x)
 	{
-	    int count=0;
+	 int count=0;
 	 unordered_map<int,int>mp;
 	 for(int i=0;i<n;i++){
 	     for(int j=0;j<n;j++){
