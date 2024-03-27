b6e9512923602a3bab8ba500556acdf1c28abeeb
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-03-28 00:30:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    int padovanSequence(int n)
+    {
+       if(n==0||n==1||n==2)
+        return 1;
+        else
+        return padovanSequence(n-2)+padovanSequence(n-3);
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout<<ob.padovanSequence(n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
