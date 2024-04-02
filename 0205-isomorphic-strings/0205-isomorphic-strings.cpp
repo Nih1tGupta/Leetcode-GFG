f519b7260e594f647e2f84367071492e2f323231
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-04-01 21:28:06 -0700

    Time: 13 ms (7.05%), Space: 11.6 MB (5.05%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,9 +5,8 @@ public:
         unordered_map<char,vector<int>>mp1;
          unordered_map<char,vector<int>>mp2;
         for(int i=0;i<s.length();i++){mp1[s[i]].push_back(i);}
-         for(int j=0;j<t.length();j++){mp2[t[j]].push_back(j);}
-        
-        for(int i=0;i<s.length();i++){
+         for(int i=0;i<t.length();i++){mp2[t[i]].push_back(i);}
+        for(int i=0;i<t.length();i++){
             if(mp1[s[i]]!=mp2[t[i]]){return false;}
         }
         return true;
