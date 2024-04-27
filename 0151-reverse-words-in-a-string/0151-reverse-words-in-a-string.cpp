21a707065da527f056a50514aea5dd17bc9a1e29
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-04-27 10:54:28 +0530

    Time: 3 ms (82.62%), Space: 8.5 MB (81.10%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string words; string ans;
-       
-        int n =s.length()-1;
-        for(int i=n; i>=0; i--)
-        {
-            if(s[i]!=' ')
-            {
-                words.push_back(s[i]);
-                    if((i>0 && s[i-1]==' ') || (i==0))
-                    {
-                        for(int j=words.length()-1; j>=0; j--)
-                        {
-                            ans.push_back(words[j]);
-                        }
-                        ans.push_back(' ');
-                        words.clear();}}}
-        ans.pop_back();
-        return ans;
-    }
+        reverse(s.begin(),s.end());
+        int i=0;
+        int l=0;
+        int r=0;
+        while(i<s.length()){
+            while(i<s.length() and s[i]!=' '){s[r]=s[i];r++;i++;}
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r]=' ';r++;l=r;
+            }
+            i++;
+        }
+        s=s.substr(0,r-1);
+        return s;
+    } 
+    // blue_is_sky_the
+    //  l    r
 };
-
-
-
-
-
-
-
-
-
+// i
