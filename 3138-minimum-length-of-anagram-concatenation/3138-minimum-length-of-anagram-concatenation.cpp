2a46c8f0d157e9a9fe356559703cb38c2e7c9dbe
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-05-09 13:04:05 +0530

    Time: 58 ms (45.32%), Space: 30.5 MB (37.06%) - LeetHub

---
+++ 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    bool solve(string s, int k) {
+        int n = s.length();
+        int cnt[26] = {0};
+        for (int i = 0; i < k; i++)
+            cnt[s[i] - 'a']++;
+        for (int i = k; i < n; i += k) {
+            int cnt2[26] = {0};
+            for (int j = i; j < i + k; j++)
+                cnt2[s[j] - 'a']++;
+            for (int j = 0; j < 26; j++)
+                if (cnt[j] != cnt2[j])
+                    return false;
+        }
+        return true;
+    }
+    int minAnagramLength(string s) {
+        int n=s.length();
+        for(int i=1;i<n;i++){
+            if(n%i==0 and solve(s,i)){return i;}
+        }
+        return n;
+    }
+};
