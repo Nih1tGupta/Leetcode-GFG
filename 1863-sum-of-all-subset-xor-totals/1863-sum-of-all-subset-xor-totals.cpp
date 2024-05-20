4a180e9827a5153eb78d1a93511637ca0b946fe0
Nihit Gupta <119615342+Nih1tGupta@users.noreply.github.com>
2024-05-20 12:21:48 +0530

    Time: 2 ms (73.08%), Space: 8.3 MB (65.34%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>&sub,vector<vector<int>>&subset,int idx){
-        if(idx==nums.size()){subset.push_back(sub);return ;}
-        sub.push_back(nums[idx]);
-        solve(nums,sub,subset,idx+1);
-        sub.pop_back();
-        solve(nums,sub,subset,idx+1);
+    int  solve(vector<int>&nums,int idx,int xorr){
+        if(idx==nums.size()){return xorr ;}
+       
+        int take=solve(nums,idx+1,nums[idx]^xorr);
+       
+        int nottake=solve(nums,idx+1,xorr);
+        return take+nottake;
     }
     int subsetXORSum(vector<int>& nums) {
-        long sum=0;
-         vector<vector<int>> subset;
-        vector<int>sub;
-        solve(nums,sub,subset,0);
-        for(auto it:subset){
-            int summ=0;
-            for(int num:it){summ^=num;}
-            sum+=summ;
-        }
-        return sum;
+        return solve(nums,0,0);
+       
     }
 };
