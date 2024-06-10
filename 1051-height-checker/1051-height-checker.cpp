b1f8d617185088203a961ef2a632ea615a288bb7
Nihit Gupta <nihit9500@gmail.com>
2024-06-10 18:36:06 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (44.04%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+         vector<int>v=heights;
+        sort(v.begin(),v.end());
+        int count=0;
+        for(int i=0;i<heights.size();i++)
+        if(v[i]!=heights[i]){ count++;}
+        return count;
+    }
+};
