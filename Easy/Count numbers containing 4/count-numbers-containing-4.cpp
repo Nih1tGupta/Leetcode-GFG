21ee11c089d9548913b004d4752b4918d4a73041
Nihit Gupta <nihit9500@gmail.com>
2024-06-12 00:09:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count numbers containing 4/count-numbers-containing-4.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        int c=0;
+        for(int i=0;i<=n;i++){
+            string s=to_string(i);
+            for(auto &it:s){
+                if(it=='4'){c++;break;}
+            }
+        }
+        return c;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        int res = obj.countNumberswith4(n);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
