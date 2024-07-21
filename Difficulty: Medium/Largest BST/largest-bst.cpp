1bcf385758d7f513449799cf8f5561f1dfc1512a
Nihit Gupta <nihit9500@gmail.com>
2024-07-22 00:05:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.cpp
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -98,7 +98,8 @@ struct Node {
         left = right = NULL;
     }
 };*/
-    class NodeValue{
+
+ class NodeValue{
         public :
         int maxNode,minNode,maxSize;
         NodeValue(int minNode,int maxNode,int maxSize){
