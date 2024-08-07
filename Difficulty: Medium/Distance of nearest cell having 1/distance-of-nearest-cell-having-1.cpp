16607c2b8ea3115fd8860c6a33cabdc96570af1b
Nihit Gupta <nihit9500@gmail.com>
2024-08-07 23:58:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -7,54 +7,40 @@ using namespace std;
 class Solution {
   public:
     // Function to find distance of nearest 1 in the grid for each cell.
+      
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
         // Code here
-     int n = grid.size(); 
-	    int m = grid[0].size(); 
-	    // visited and distance matrix
-	    vector<vector<int>> vis(n, vector<int>(m, 0)); 
-	    vector<vector<int>> dist(n, vector<int>(m, 0)); 
-	    // <coordinates, steps>
-	    queue<pair<pair<int,int>, int>> q; 
-	    // traverse the matrix
-	    for(int i = 0;i<n;i++) {
-	        for(int j = 0;j<m;j++) {
-	            // start BFS if cell contains 1
-	            if(grid[i][j] == 1) {
-	                q.push({{i,j}, 0}); 
-	                vis[i][j] = 1; 
-	            }
-	            else {
-	                // mark unvisited 
-	                vis[i][j] = 0; 
-	            }
-	        }
-	    }
-	    
-	    int delrow[] = {-1, 0, +1, 0}; 
-	    int delcol[] = {0, +1, 0, -1}; 
-	    
-	    // traverse till queue becomes empty
-	    while(!q.empty()) {
-	        int row = q.front().first.first; 
-	        int col = q.front().first.second; 
-	        int steps = q.front().second; 
-	        q.pop(); 
-	        dist[row][col] = steps; 
-	        // for all 4 neighbours
-	        for(int i = 0;i<4;i++) {
-	            int nrow = row + delrow[i]; 
-	            int ncol = col + delcol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-	            && vis[nrow][ncol] == 0) {
-	                vis[nrow][ncol] = 1; 
-	                q.push({{nrow, ncol}, steps+1});  
-	            }
-	        }
-	    }
-	    // return distance matrix
-	    return dist; 
+        int n=grid.size();int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>dis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        // q.push( {{0,0},0});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                  q.push( {{i,j},0});
+                  vis[i][j]=1;
+                }
+            }
+        }
+        // int steps=0;
+        int dir[]={-1,0,1,0};
+        int dic[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+            q.pop();
+            dis[row][col]=steps;
+            for(int i=0;i<4;i++){
+                int nr=row+dir[i];
+                int nc=col+dic[i];
+                if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc]){
+                    vis[nr][nc]=1;q.push({{nr,nc},steps+1});
+                }
+            }
+        }
+        return dis;
     }
 };
 
