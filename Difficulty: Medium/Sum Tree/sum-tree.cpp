2da82bf3d6cf631ee2a429b2130b70c0c58324a4
Nihit Gupta <nihit9500@gmail.com>
2024-08-08 00:02:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -2,83 +2,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node {
     int data;
-    struct Node *left;
-    struct Node *right;
+    struct Node* left;
+    struct Node* right;
 };
+
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
+
     return temp;
 }
+
 // Function to Build Tree
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
@@ -92,39 +91,37 @@ struct Node
 }; */
 
 // Should return true if tree is Sum Tree, else false
-class Solution
-{
-    public:
+class Solution {
+ public:
     int sum(Node*root){
         if(root==nullptr){return 0;}
-        return (root->data +sum(root->left)+sum(root->right));
+        return root->data +sum(root->left)+sum(root->right);
     }
     bool isSumTree(Node* root)
     {
-         // Your code here
-         if(root==nullptr || root->left==nullptr and root->right==nullptr){return true;}
-         int ls=sum(root->left);int rs=sum(root->right);
-         bool la=isSumTree(root->left);
-           bool ra=isSumTree(root->right);
-         if(ls+rs==root->data and la and ra){return true;}
-         return false;
+        if(root==nullptr ||(root->left ==nullptr  and root->right==nullptr)){return true;}
+        int left=sum(root->left); int right=sum(root->right);
+        return (root->data== left+right) and (isSumTree(root->left) and isSumTree(root->right));
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
 
     int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-		getline(cin,s);
+
+        getline(cin, s);
+
         Node* root = buildTree(s);
         Solution ob;
-        cout <<ob.isSumTree(root) << endl;
+        if (ob.isSumTree(root))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
     }
     return 1;
 }
