c8f1ae0fe5c46a1ebefeb065b211cdee5608b80d
Nihit Gupta <nihit9500@gmail.com>
2024-09-07 21:04:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nth Natural Number/nth-natural-number.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long findNth(long long n) {
+        // code here.
+        long long ans=0;long long  mul=1;
+        while(n>0){
+            ans+=(n%9)*mul;
+            n/=9;
+            mul*=10;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, ans;
+        cin >> n;
+        Solution obj;
+        ans = obj.findNth(n);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
