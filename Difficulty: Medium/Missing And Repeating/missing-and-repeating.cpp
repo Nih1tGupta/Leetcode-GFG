a48939ae91930ac93c1001b7e135f68cc6beea9f
Nihit Gupta <nihit9500@gmail.com>
2024-09-23 00:20:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        unordered_map<int,int>mp;vector<int>v;
+      for(int i=0;i<n;i++){mp[arr[i]]++;}
+        for(auto it:mp){if(it.second ==2){v.push_back(it.first);break;}
+        }
+        for(int i=1;i<=n;i++){
+            if(mp.find(i)==mp.end()){v.push_back(i);break;}
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
