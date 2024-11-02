3244d0669b55c33e72b7d6c36c910cd29b4191af
Nihit Gupta <nihit9500@gmail.com>
2024-11-02 10:05:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.91%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        int n=s.length();
+          if (s[0] != s[n - 1]) {
+            return false;
+        }
+        for (int i = 1; i < n; ++i) {
+            if (s[i] == ' ') {
+                if (s[i - 1] != s[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
