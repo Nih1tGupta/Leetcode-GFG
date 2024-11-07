dfc4f17ee84c80097ca720bfba22991959cd1a56
Nihit Gupta <nihit9500@gmail.com>
2024-11-08 00:31:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum repeat to make substring/minimum-repeat-to-make-substring.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minRepeats(string& s1, string& s2) {
+        // code here
+          int count = 1;
+        string temp =s1;
+        for(int i=0; i<800; i++){
+            if(temp.find(s2) != string::npos){
+                return count;
+            }
+            temp += s1;
+            count ++;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string A, B;
+        getline(cin, A);
+        getline(cin, B);
+
+        Solution ob;
+        cout << ob.minRepeats(A, B) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
