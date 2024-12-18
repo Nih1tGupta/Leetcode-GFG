79efc005d6e0491e43cf3f227249c931472a1dd8
Nihit Gupta <nihit9500@gmail.com>
2024-12-18 11:27:41 +0530

    Time: 0 ms (100%), Space: 14.3 MB (9.86%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,11 +3,21 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         int n=prices.size();
         // vector<int>v(n,0);
-        for(int i=0;i<n;i++){
-            int mini=prices[i];
-            for(int j=i+1;j<n;j++){
-                if(mini>=prices[j]){prices[i]-=prices[j];break;}
+        // for(int i=0;i<n;i++){
+        //     int mini=prices[i];
+        //     for(int j=i+1;j<n;j++){
+        //         if(mini>=prices[j]){prices[i]-=prices[j];break;}
+        //     }
+        // }
+        // return prices;
+        // NEXT SMALLER ELEMENT
+        stack<int> stk;
+        for(int i=0; i<n; i++){
+            while(!stk.empty() && prices[stk.top()] >= prices[i]){
+                prices[stk.top()] -= prices[i];
+                stk.pop();
             }
+            stk.push(i);
         }
         return prices;
     }
