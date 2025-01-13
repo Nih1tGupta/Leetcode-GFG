41533e6f3fe188ee6863d1ecd6499123945ad83b
Nihit Gupta <nihit9500@gmail.com>
2025-01-13 17:59:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -9,45 +9,35 @@ using namespace std;
 
 class Solution {
   public:
-  
-   vector<pair<int,int>>directions = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
-    void dfs(int i , int j , vector<vector<int>>& vis , vector<vector<int>>& grid
-    , set<vector<pair<int,int>>>& st , int r0 , int c0,vector<pair<int,int>>&vec)
-    {
-        vis[i][j] = 1;
-        vec.push_back({i - r0 , j - c0});
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        for(auto dir : directions){
-                int nr = i + dir.first;
-                int nc = j + dir.second;
-                if(nr >= 0 && nr < n && nc >= 0 && nc <m && grid[nr][nc] == 1 
-                && !vis[nr][nc])
-                dfs(nr , nc , vis ,grid , st , r0 , c0,vec);
+    void dfs(vector<vector<int>>& grid,vector<vector<int>>& vis,int r,int c,vector<pair<int,int>>&v,int r0,int c0,set<vector<pair<int,int>>>&st){
+        vis[r][c]=1;
+        int n=grid.size(); int m=grid[0].size();
+        v.push_back({r-r0,c-c0});
+        int dr[4]={-1,0,1,0};
+        int dc[4]={0,-1,0,+1};
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i]; int nc=c+dc[i];
+            if(nr>=0 and nr<n and nc>=0 and nc<m and vis[nr][nc]==0 and grid[nr][nc]==1){
+                dfs(grid,vis,nr,nc,v,r0,c0,st);
+            }
         }
+        
     }
     int countDistinctIslands(vector<vector<int>>& grid) {
-        // code here
         int n=grid.size(); int m=grid[0].size();
-        // vector<vector<int>>vis(n,vector<int>(m,0));
-        set<vector<pair<int,int>>>st;
-        vector<vector<int>>vis(n , vector<int>(m,0));
-        
-        for(int i = 0; i<n; i++)
-        {
-            for(int j  =0; j<m ; j++)
-            {
-                if(!vis[i][j] && grid[i][j] == 1)
-                {
-                    vector<pair<int,int>> vec;
-                    dfs(i,j,vis,grid,st, i , j,vec);
-                    st.insert(vec);
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        set<vector<pair<int,int>>>s;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] and grid[i][j]==1){
+                    vector<pair<int,int>>v;
+                    dfs(grid,vis,i,j,v,i,j,s);
+                    s.insert(v);
+                    
                 }
             }
         }
-        return st.size();
-        
+        return s.size();
     }
 };
 
@@ -69,6 +59,8 @@ int main() {
         }
         Solution obj;
         cout << obj.countDistinctIslands(grid) << endl;
-    }
+    
+cout << "~" << "\n";
+}
 }
 // } Driver Code Ends
