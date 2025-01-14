ab7e0dc73a83f4cccce7c23baaefb121388f05e7
Nihit Gupta <nihit9500@gmail.com>
2025-01-14 12:55:38 +0530

    Time: 1 ms (92.23%), Space: 11.6 MB (85.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int count=0;
-     void calculate(vector<int>& nums, int i, int sum, int S) {
-        if (i == nums.size()) {
-            if (sum == S) {
-                count++;
-            }
-        } else {
-            calculate(nums, i + 1, sum + nums[i], S);
-            calculate(nums, i + 1, sum - nums[i], S);
+  int dp[21][2001]; 
+    int f(int i, vector<int>& nums, int t, int curr) {
+        if (i < 0) {
+            return curr == t ? 1 : 0;
         }
+        if (dp[i][curr + 1000] != -1) return dp[i][curr + 1000];
+        int add = f(i - 1, nums, t, curr + nums[i]);
+        int subtract = f(i - 1, nums, t, curr - nums[i]);
+        
+        return dp[i][curr + 1000] = add + subtract;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-          calculate(nums, 0, 0, target);
-        return count;
+    int findTargetSumWays(vector<int>& nums, int t) {
+        int n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        return f(n-1,nums,t,0);
     }
 };
