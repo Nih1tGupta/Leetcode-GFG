163c2431f41eb40767a35e5ac4df11b7edd454be
Nihit Gupta <nihit9500@gmail.com>
2025-01-14 12:56:49 +0530

    Time: 7 ms (77.5%), Space: 11.6 MB (73.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
   int dp[21][2001]; 
     int f(int i, vector<int>& nums, int t, int curr) {
-        if (i < 0) {
-            return curr == t ? 1 : 0;
-        }
+        if (i == 0) {
+    if (nums[0] == 0) {
+        // If the first element is 0, both +0 and -0 lead to the same target
+        return t == curr ? 2 : 0;
+    }
+    // Check if either +nums[0] or -nums[0] can satisfy the target
+    return (t == curr + nums[0]) + (t == curr - nums[0]);
+}
         if (dp[i][curr + 1000] != -1) return dp[i][curr + 1000];
         int add = f(i - 1, nums, t, curr + nums[i]);
         int subtract = f(i - 1, nums, t, curr - nums[i]);
