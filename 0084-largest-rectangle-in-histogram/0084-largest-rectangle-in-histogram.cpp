3c297cf5cd8b4598f2048c47e982f52c098b7a45
Nihit Gupta <nihit9500@gmail.com>
2025-01-16 11:59:24 +0530

    Time: 7 ms (98.28%), Space: 81.4 MB (67.07%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
-       vector<int>getnsr(vector<int>&height){
+    int largestRectangleArea(vector<int>& h) {
+        int maxi=0;int n=h.size();
         stack<int>st;
-         int n=height.size();
-            vector<int>NSR(n);
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){NSR[i]=n;}
-            else{
-                while(!st.empty() and height[st.top()]>=height[i]){st.pop();}
-                if(st.empty()){NSR[i]=n;}
-                else{NSR[i]=st.top();}
+        for(int i=0;i<h.size();i++){
+            while(!st.empty() and h[i]<h[st.top()]){
+                int element=st.top();
+                st.pop();
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
+                maxi=max(maxi,h[element]*(nse-pse-1));
             }
             st.push(i);
         }
-            return NSR;                             
-    }                           
-    vector<int>getnsl(vector<int>&height){
-          stack<int>st;
-         int n=height.size();
-            vector<int>NSL(n);
-        for(int i=0;i<n;i++){
-            if(st.empty()){NSL[i]=-1;}
-            else{
-                while(!st.empty() and height[st.top()]>=height[i]){st.pop();}
-                if(st.empty()){NSL[i]=-1;}
-                else{NSL[i]=st.top();}
-            }
-            st.push(i);
-        }
-            return NSL;   
+    // leftover -they have no nse;
+    while(!st.empty()){
+        int ele=st.top();
+        int nse=n;
+        st.pop();
+        int pse=st.empty()?-1:st.top();
+        maxi=max(maxi,h[ele]*(nse-pse-1));
     }
-    int maxarea(vector<int>&height){
-        
-        vector<int>NSR=getnsr(height);
-         vector<int>NSL=getnsl(height);
-        int n=height.size();
-        vector<int>width(n);
-        for(int i=0;i<n;i++){width[i]=(NSR[i]-NSL[i]-1);}
-        int maxii=0;
-        for(int i=0;i<n;i++){
-            int a=width[i]*height[i];
-            maxii=max(maxii,a);
-        }
-        return maxii;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        int maxi= maxarea(heights);
-        return maxi;
+    return maxi;
     }
 };
