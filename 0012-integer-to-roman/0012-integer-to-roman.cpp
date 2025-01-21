89ab91ee5913b72aadbadfea8d44511ec19f181a
Nihit Gupta <nihit9500@gmail.com>
2025-01-21 09:21:47 +0530

    Time: 0 ms (100%), Space: 9.4 MB (68.32%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    
+   
     string intToRoman(int num) {
-         string romanSymbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
+         string Symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-
-        string ans="";
-        // jb convert krte h to sbse bda number choose krte hai
+        string s="";
         for(int i=0;i<13;i++){
             while(num>=values[i]){
-                ans+=romanSymbols[i];
+                s+=Symbol[i];
                 num-=values[i];
             }
         }
-        return ans;
+        return s;
     }
 };
