640be0c74b687467885cecc83ec9576781bb7cbb
Nihit Gupta <nihit9500@gmail.com>
2025-02-10 16:54:06 +0530

    Time: 0 ms (100%), Space: 8.8 MB (52.35%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -2,21 +2,30 @@ class Solution {
 public:
     string clearDigits(string s) {
         int sz=s.length();
-        for(int i=sz-1;i>=0;i--){
-            if(s[i]>='0' and s[i]<='9'){
-                s[i]='*';
-                int j=i-1;
-                while(j>=0){
-                    if(s[j]>='a' and s[j]<='z'){s[j]='*';break;}
-                    j--;
-                }
-                }
-        }
-        string  ss;
-        for(int i=0;i<sz;i++){
-            if(s[i]=='*')continue;
-            ss+=s[i];
-        }
-        return ss;
+    //     for(int i=sz-1;i>=0;i--){
+    //         if(s[i]>='0' and s[i]<='9'){
+    //             s[i]='*';
+    //             int j=i-1;
+    //             while(j>=0){
+    //                 if(s[j]>='a' and s[j]<='z'){s[j]='*';break;}
+    //                 j--;
+    //             }
+    //             }
+    //     }
+    //     string  ss;
+    //     for(int i=0;i<sz;i++){
+    //         if(s[i]=='*')continue;
+    //         ss+=s[i];
+    //     }
+    //     return ss;
+    stack<char>st;
+    for(int i=0;i<sz;i++){
+     if(isalpha(s[i]))st.push(s[i]);
+     else{st.pop();}   
+    }
+    string ss;
+    while(!st.empty()){ss+=st.top();st.pop();}
+    reverse(ss.begin(),ss.end());
+    return ss;
     }
 };
