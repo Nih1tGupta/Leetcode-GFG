73ddba66390a9b7e6b647f7bac61a9a69a1e16ae
Nihit Gupta <nihit9500@gmail.com>
2025-02-15 00:20:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
+++ Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -2,124 +2,139 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+class Node {
+  public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
+    // Constructor to initialize a new node
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = NULL;
+        right = NULL;
     }
 };
+
 // Function to Build Tree
 
 
 // } Driver Code Ends
-//Function to find the lowest common ancestor in a BST.
-class Solution{
-    public:
-        Node* LCA(Node *root, int n1, int n2)
-        {
-            // code here
-            if(root==nullptr)return nullptr;
-            if(root->data==n1 || root->data==n2){
-                return root;
-            }
-            Node*la=LCA(root->left,n1,n2);
-            Node*ra=LCA(root->right,n1,n2);
-            // conditions
-            if(la!=nullptr and ra==nullptr)return la;
-            if(la==nullptr and ra!=nullptr)return ra;
-            if(la==nullptr and ra==nullptr)return nullptr;
-            else if(la!=nullptr and ra!=nullptr){return root;}
-        }
+/*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
 
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        if(root==nullptr)return nullptr;
+        if(root->data==n1->data || root->data==n2->data)return root;
+        Node* a = LCA(root->left,n1,n2);
+        Node* b=LCA(root->right,n1,n2);
+        if(a!=nullptr and b==nullptr)return a;
+        if(a==nullptr and b!=nullptr)return b;
+        if(a==nullptr and b==nullptr)return nullptr;
+        else if(a!=nullptr and b!=nullptr){return root;}
+        
+    }
 };
 
 
 //{ Driver Code Starts.
 
-Node* buildTree(string str)
-{   
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = new Node(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the left child for the current node
             currNode->left = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
+
             // Create the right child for the current node
             currNode->right = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
-
-int main()
-{
+int main() {
 
     int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-        int l , h;
-		getline(cin,s);
-		scanf("%d ",&l);
-		scanf("%d ",&h);
+        int l, h;
+        getline(cin, s);
+        scanf("%d ", &l);
+        scanf("%d ", &h);
+        Node* n1 = new Node(l);
+        Node* n2 = new Node(h);
         Node* root = buildTree(s);
         Solution sol;
-        int ans = sol.LCA(root, l, h)->data;
-        cout<<ans<<endl;
+        int ans = sol.LCA(root, n1, n2)->data;
+        cout << ans << endl;
+        cout << "~"
+             << "\n";
     }
     return 1;
 }
