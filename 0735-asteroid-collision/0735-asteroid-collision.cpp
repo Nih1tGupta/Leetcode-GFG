049e9a5b8f75474f617c3a1b769de1afeedd7a65
Nihit Gupta <nihit9500@gmail.com>
2025-02-14 22:03:21 +0530

    Time: 0 ms (100%), Space: 21.5 MB (88.95%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        int n = asteroids.size();
+        for(int i = 0; i<n; i++) {
+            if(asteroids[i] > 0) st.push_back(asteroids[i]);
+            else {
+                while(!st.empty() && st.back() > 0 && st.back()<abs(asteroids[i]))
+                    st.pop_back();
+                if(!st.empty() && st.back() == abs(asteroids[i])) 
+                    st.pop_back();
+                else if(st.empty() || st.back()<0)
+                    st.push_back(asteroids[i]); 
+            }
+        }
+        return st;
+    }
+};
