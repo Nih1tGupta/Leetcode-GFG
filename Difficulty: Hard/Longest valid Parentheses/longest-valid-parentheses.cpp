a56fc73800e190b71249a72085c2ab07ac63ff1e
Nihit Gupta <nihit9500@gmail.com>
2025-02-22 15:32:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -10,23 +10,38 @@ using namespace std;
 class Solution {
   public:
     int maxLength(string& s) {
-       stack<int>st;
-      st.push(-1);
-       int maxi=0;
-       for(int i=0;i<s.length();i++){
-           if(s[i]=='('){st.push(i);}
-           else{
-               st.pop();
-               if(st.empty()){st.push(i);}
-               else{
-                   int len=i-st.top();
-                   maxi=max(maxi,len);
-               }
+    //   stack<int>st;
+    //   st.push(-1);
+    //   int maxi=0;
+    //   for(int i=0;i<s.length();i++){
+    //       if(s[i]=='('){st.push(i);}
+    //       else{
+    //           st.pop();
+    //           if(st.empty()){st.push(i);}
+    //           else{
+    //               int len=i-st.top();
+    //               maxi=max(maxi,len);
+    //           }
             
-           }
+    //       }
            
-       }
-       return maxi;
+    //   }
+    //   return maxi;
+    int n=s.length();
+    vector<int>dp(n,0);
+    for(int i=0;i<s.length();i++){
+            if(s[i]==')'){
+                if(i-1>=0){
+                    if(s[i-1]=='('){dp[i]=dp[max(i-2,0)]+2;}
+                    else{
+                        if(i-1-dp[i-1]>=0 and s[i-1-dp[i-1]]=='('){
+                            dp[i]=dp[i-1]+2+dp[max(0,i-1-dp[i-1]-1)];
+                        }
+                    }
+            }
+            }
+    }
+    return *max_element(dp.begin(),dp.end());
     }
 };
 
