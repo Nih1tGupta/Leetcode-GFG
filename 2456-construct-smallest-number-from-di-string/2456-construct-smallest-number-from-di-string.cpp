1105da168da6bdc4bd6d2376159fad157c232c18
Nihit Gupta <nihit9500@gmail.com>
2025-02-24 15:53:26 +0530

    Time: 0 ms (100%), Space: 9.3 MB (6.65%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-    bool match(string &p,string &s){
-        for(int i=0;i<p.length();i++){
-            if( (p[i]=='I' and s[i]>s[i+1]) || (p[i]=='D' and s[i]<s[i+1]) ){return 0;}
+    // bool match(string &p,string &s){
+    //     for(int i=0;i<p.length();i++){
+    //         if( (p[i]=='I' and s[i]>s[i+1]) || (p[i]=='D' and s[i]<s[i+1]) ){return 0;}
+    //     }
+    //     return 1;
+    // }
+
+    bool solve(int i,string &p,string &num,vector<bool>&vis){
+        if(num.size()==p.size()+1)return 1;
+        for(int j=1;j<=9;j++){
+            if(vis[j])continue;
+            if( (p[i]=='I' and num.back()-'0'<j) || (p[i]=='D' and num.back()-'0'>j )){
+                num+=to_string(j);
+                vis[j]=true;
+                if(solve(i+1,p,num,vis))return 1;
+                vis[j]=false;
+                num.pop_back();
+            }
         }
-        return 1;
+        return false;
     }
     string smallestNumber(string pattern) {
-        string ss="";
-        int n=pattern.length();
-        for(int i=1;i<=n+1;i++){
-            ss.push_back(i+'0');
-        }
-        while(!match(pattern,ss)){
-            next_permutation(begin(ss),end(ss));
-        }return ss;
+        // string ss="";
+        // int n=pattern.length();
+        // for(int i=1;i<=n+1;i++){
+        //     ss.push_back(i+'0');
+        // }
+        // while(!match(pattern,ss)){
+        //     next_permutation(begin(ss),end(ss));
+        // }return ss;
+        // O(len)*(n+1)!
+
+
+// recursion 
+    vector<bool>vis(10,0);
+    for(int i=1;i<=9;i++){
+        string s=to_string(i);
+        vis[i]=1;
+        if(solve(0,pattern,s,vis))return s;
+        vis[i]=false;
+    }
+    return "";
     }
 };
