6178d29a4eb35e1e01970ce1e3279f8c56982cea
Nihit Gupta <nihit9500@gmail.com>
2025-02-27 13:35:06 +0530

    Time: 271 ms (79.7%), Space: 16.4 MB (56.62%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         int maxi=0;int n=arr.size();
        unordered_map<long long,bool>mp;
        for(auto it:arr){mp[it]=1;}
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 long long f=arr[i];  long long s=arr[j];
                 int curr=2;
