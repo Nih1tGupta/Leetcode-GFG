2a4d3a2ad4d4d90b0df9286fc591bd764387549e
Nihit Gupta <nihit9500@gmail.com>
2025-02-28 12:57:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.length(); int m=str2.length();
-        vector<vector<int>>v(n+1,vector<int>(m+1));
+        vector<vector<int>>v(n+1,vector<int>(n+1));
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
                 if(i==0||j==0){v[i][j]=i+j;}
