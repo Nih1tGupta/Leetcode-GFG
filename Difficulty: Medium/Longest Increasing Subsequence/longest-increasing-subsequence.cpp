f41a45344f083f6b5f5edfa9ef38c6df560c78ca
Nihit Gupta <nihit9500@gmail.com>
2025-03-04 15:40:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -7,22 +7,34 @@ using namespace std;
 
 class Solution {
   public:
-    int dp[1001][1001];
-    int solve(vector<int>&arr,int i,int p){
-        if(i>=arr.size()){return 0;}
-        if(dp[i][p]!=-1 and p!=-1){return dp[i][p];}
-        int t=0;
-        if(p==-1||arr[i]>arr[p]){
-            t=1+solve(arr,i+1,i);
-        }
-        int nt=solve(arr,i+1,p);
-        if(p!=-1){dp[i][p]=max(t,nt);}
-        return max(t,nt);
-    }
+    // int dp[1001][1001];
+    // int solve(vector<int>&arr,int i,int p){
+    //     if(i>=arr.size()){return 0;}
+    //     if(dp[i][p]!=-1 and p!=-1){return dp[i][p];}
+    //     int t=0;
+    //     if(p==-1||arr[i]>arr[p]){
+    //         t=1+solve(arr,i+1,i);
+    //     }
+    //     int nt=solve(arr,i+1,p);
+    //     if(p!=-1){dp[i][p]=max(t,nt);}
+    //     return max(t,nt);
+    // }
     int lis(vector<int>& arr) {
         // code here
-        memset(dp,-1,sizeof(dp));
-        return solve(arr,0,-1);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(arr,0,-1);
+        int n=arr.size();
+        vector<int>dp(n+1,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<=i-1;prev++){
+                if(arr[prev]<arr[i]){
+                    dp[i]=max(dp[i],1+dp[prev]);
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
 
