e1b36e90a9771f69173d7ad28476d929e4829362
Nihit Gupta <nihit9500@gmail.com>
2025-03-09 12:44:39 +0530

    Time: 4 ms (60%), Space: 49 MB (80%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& f, vector<int>& b) {
-        int c=0;int n=f.size();
-        int m=b.size();
-        vector<int>vis(n,0);
-        vector<int>visb(m,0);
+        int up=0;
         for(int i=0;i<f.size();i++){
+            int ele=f[i];
             for(int j=0;j<b.size();j++){
-                if( (vis[i]==0 and visb[j]==0) and (f[i]<=b[j]) ){visb[j]=1;vis[i]=1;c++;}
+                if(b[j]>=ele){
+                    b[j]=-1;
+                    break;
+                }
+                if(j==b.size()-1){up++;}
             }
         }
-        return f.size()-c;
+        return up;
     }
 };
