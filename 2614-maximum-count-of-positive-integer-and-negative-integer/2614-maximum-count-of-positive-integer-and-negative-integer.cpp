71f1a6676adac2c9d5ffcdfcbd4edc037d33c0ec
Nihit Gupta <nihit9500@gmail.com>
2025-03-12 11:13:54 +0530

    Time: 0 ms (100%), Space: 21.3 MB (75.85%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,11 +1,41 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int n,p=0;
-        for(auto it:nums){
-            if(it<0){n++;}
-            else if(it>0){p++;}
+    int lb(vector<int>&nums){
+         int idx=nums.size();
+        int l=0;int h=idx-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(nums[m]<0){l=m+1;}
+            else if(nums[m]>=0){
+                h=m-1;
+                idx=m;
+                }
         }
-        return max(n,p);
+        return idx;
+    }
+    int ub(vector<int>&nums){
+         int idx=nums.size();
+        int l=0;int h=idx-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(nums[m]<=0){l=m+1;}
+            else if(nums[m]>0){
+                h=m-1;
+                idx=m;
+                }
+        }
+        return idx;
+    }
+    int maximumCount(vector<int>& nums) {
+        // int n,p=0;
+        // for(auto it:nums){
+        //     if(it<0){n++;}
+        //     else if(it>0){p++;}
+        // }
+        // return max(n,p);
+        // BINARY SEARCH
+       int p=nums.size()-ub(nums);
+       int n=lb(nums);
+       return max(n,p);
     }
 };
