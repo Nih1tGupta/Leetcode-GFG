6e6f807e8bd2c33de1f0a31bfdec1febe353fd13
Nihit Gupta <nihit9500@gmail.com>
2025-03-13 23:21:38 +0530

    Time: 9 ms (20.29%), Space: 18.5 MB (49.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,54 +1,36 @@
 class Solution {
 public:
-    vector<int> topoSortBfs(int n, unordered_map<int, list<int>>& adjList) {
-        vector<int> result;
-        queue<int> q;
-        unordered_map<int, int> indegree;
-
-        for (auto& pair : adjList) {
-            int node = pair.first;
-            for (auto& neighbor : pair.second) {
-                indegree[neighbor]++;
+    vector<int>bfs(vector<vector<int>>adj,int n){
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
+        queue<int>q;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
         }
-
-        while (!q.empty()) {
-            int currentNode = q.front();
-            q.pop();
-
-            result.push_back(currentNode);
-
-            for (auto& neighbor : adjList[currentNode]) {
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
+        while(!q.empty()){
+            int front=q.front(); q.pop();
+            v.push_back(front);
+            for(auto it:adj[front]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
                 }
             }
         }
-
-        if (result.size() == n) {
-            return result;
-        } else {
-            return {};
-        }
+        return v;
     }
-
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, list<int>> adjList;
-
-        for (auto& prerequisite : prerequisites) {
-            int u = prerequisite[0];
-            int v = prerequisite[1];
-            adjList[v].push_back(u);
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        vector<vector<int>>v(n);
+        for(int i=0;i<p.size();i++){
+            v[p[i][1]].push_back(p[i][0]);
         }
-
-        return topoSortBfs(numCourses, adjList);
-    
+        vector<int>topo=bfs(v,n);
+        if(topo.size()==n)return topo;
+        return {};
     }
 };
