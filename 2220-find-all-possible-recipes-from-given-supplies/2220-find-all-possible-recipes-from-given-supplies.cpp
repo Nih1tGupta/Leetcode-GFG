fed9d4955bbeae367abfa5f4eb743209fd348813
Nihit Gupta <nihit9500@gmail.com>
2025-03-21 19:42:40 +0530

    Time: 25 ms (97.82%), Space: 101 MB (89.42%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,56 +1,38 @@
 class Solution {
 public:
-    // bool possible( vector<string>& ing, vector<string>& supplies){
-    //      int sz=ing.size();int n=supplies.size();
-    //      int c=0;
-    //      for(int i=0;i<sz;i++){
-    //         for(int j=0;j<n;j++){
-    //             if(ing[i]==supplies[j]){c++;}
-    //         }
-    //      }
-    //      return (c==sz);
-    // }
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        // vector<string>v;
-        // unordered_set<string>st;
-        // for(int i=0;i<recipes.size();i++){
-        //     string tomake=recipes[i];
-        //     if(possible(ing[i],supplies)){
-        //         v.push_back(tomake);
-        //         st.insert(tomake);
-        //         supplies.push_back(tomake);
-        //     }
-        // }
-        // for(int i=0;i<recipes.size();i++){
-        //     string tomake=recipes[i];
-        //     if(possible(ing[i],supplies)){
-        //         if(st.find(tomake)==st.end()){
-        //             v.push_back(tomake);
-        //         }
-        //     }
-        // }
-        // return v;
-        int n = recipes.size();
-        vector<string> ans;
-        unordered_set<string> st(supplies.begin(), supplies.end());
-        int cnt = n;
-        while (cnt--) {
-            for (int i = 0; i < n; i++) {
-                bool flag = true;
-                if (st.find(recipes[i]) != st.end())
-                    continue;
-                for (int j = 0; j < ingredients[i].size(); j++) {
-                    if (st.find(ingredients[i][j]) == st.end()) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    ans.push_back(recipes[i]);
-                    st.insert(recipes[i]);
+       int n = recipes.size();
+        unordered_set<string> st(begin(supplies), end(supplies));
+        unordered_map<string, vector<int>> adj; //ing ---> recipe
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(string& ing : ingredients[i]) {
+                if(!st.count(ing)) {
+                    adj[ing].push_back(i);
+                    indegree[i]++;
                 }
             }
         }
-        return ans;
+        queue<int> que;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                que.push(i);
+            }
+        }
+        vector<string> result;
+        while(!que.empty()) {
+            int i = que.front();
+            que.pop();
+            string recipe = recipes[i];
+            result.push_back(recipe);
+
+            for(int &idx : adj[recipe]) {
+                indegree[idx]--;
+                if(indegree[idx] == 0) {
+                    que.push(idx);
+                }
+            }
+        }
+        return result;
     }
 };
