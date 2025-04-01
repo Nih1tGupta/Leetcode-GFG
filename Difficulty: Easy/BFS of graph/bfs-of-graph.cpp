446123048433ee2ca674834c5d3ad05c29c2c31a
Nihit Gupta <nihit9500@gmail.com>
2025-04-02 00:53:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -2,55 +2,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        // Code here
+    vector<int> bfs(vector<vector<int>> &adj) {
         queue<int>q;
-        int vis[V]={0};
         q.push(0);
+        int n=adj.size();
+        vector<int>v;
+        vector<bool>vis(n,0);
         vis[0]=1;
-        vector<int>bfs;
+        v.push_back(0);
         while(!q.empty()){
-            int node=q.front(); q.pop();
-            bfs.push_back(node);
-            for(auto &it:adj[node]){
-                if(!vis[it]){vis[it]=1;q.push(it);}
+            int curr=q.front();q.pop();
+            for(auto it:adj[curr]){
+                if(vis[it]==0){
+                    v.push_back(it);
+                    q.push(it);
+                    vis[it]=1;
+                }
             }
         }
-        return bfs;
+        return v;
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
-        int V, E;
-        cin >> V >>
+        int V;
+        cin >> V;
+        cin.ignore();
+        // Use vector of vectors instead of array of vectors.
+        vector<vector<int>> adj(V);
 
-            E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            // 		adj[v].push_back(u);
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
         }
-        // string s1;
-        // cin>>s1;
+
         Solution obj;
-        vector<int> ans = obj.bfsOfGraph(V, adj);
+        vector<int> ans = obj.bfs(adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
         cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
