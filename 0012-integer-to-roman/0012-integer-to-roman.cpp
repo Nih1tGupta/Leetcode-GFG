37bf2429cc28fd8dc5971250182b802343e1b511
Nihit Gupta <nihit9500@gmail.com>
2025-04-07 16:44:30 +0530

    Time: 0 ms (100%), Space: 9.1 MB (89.51%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -6,10 +6,10 @@ public:
         int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string s="";
         for(int i=0;i<13;i++){
-            while(num>=values[i]){
-                s+=Symbol[i];
-                num-=values[i];
-            }
+            if(num==0)break;
+            int times=num/values[i];
+            while(times--){s+=Symbol[i];}
+            num= num%values[i];
         }
         return s;
     }
