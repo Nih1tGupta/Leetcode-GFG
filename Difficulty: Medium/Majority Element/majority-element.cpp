d9a185fd8f3a96f36ca8275a06526e881fe948ca
Nihit Gupta <nihit9500@gmail.com>
2025-04-25 22:49:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        int size=arr.size();
+         unordered_map<int,int> mp;
+        for(int i=0;i<size;i++){
+            mp[arr[i]]++;
+        }
+        for(int i=0;i<size;i++){
+            if(mp[arr[i]]>size/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
