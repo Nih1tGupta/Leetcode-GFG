62f3584d27e48159c929b1e2984656071952cbd6
Nihit Gupta <nihit9500@gmail.com>
2025-05-05 00:11:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int findTarget(vector<int>& arr, int target) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==target)return i;
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t-- > 0) {
+        string str;
+        getline(cin, str);
+
+        stringstream ss(str);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution sln;
+        int ans = sln.findTarget(arr, target);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
