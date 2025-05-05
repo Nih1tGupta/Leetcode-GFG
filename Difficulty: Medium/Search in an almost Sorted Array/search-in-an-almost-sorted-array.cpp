a4854b69aff7eff7f7b4b6cbb68ff4f2e86b36be
Nihit Gupta <nihit9500@gmail.com>
2025-05-05 10:42:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -12,8 +12,18 @@ class Solution {
   public:
     int findTarget(vector<int>& arr, int target) {
         // code here
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==target)return i;
+        // for(int i=0;i<arr.size();i++){
+        //     if(arr[i]==target)return i;
+        // }
+        // return -1;
+        int l=0;int r=arr.size()-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(arr[m]==target)return m;
+            if(m>l and arr[m-1]==target){return m-1;}
+            if(m<r and arr[m+1]==target){return m+1;}
+            if(arr[m]>target){r=m-2;}
+            else{l=m+2;}
         }
         return -1;
     }
