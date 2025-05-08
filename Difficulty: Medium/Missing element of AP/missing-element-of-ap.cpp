f36d15c63c52779146edb89020f1ee44460058d6
Nihit Gupta <nihit9500@gmail.com>
2025-05-08 09:53:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int findMissing(vector<int> &arr) {
+        // code here
+        int n =arr.size();
+        int d= min(arr[1]-arr[0], arr[2]-arr[1]);
+        for(int i=0;i<arr.size();i++){
+            if(arr[i+1]!=arr[i]+d)return arr[i]+d;
+        }
+        return arr[n-1]+d;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.findMissing(arr) << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
