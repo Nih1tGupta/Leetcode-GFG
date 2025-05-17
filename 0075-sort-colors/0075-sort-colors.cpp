0e82861f3a89502bdc6762cf294baafded339de5
Nihit Gupta <nihit9500@gmail.com>
2025-05-17 10:24:43 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         vector<int> ans;
-        priority_queue<int, vector<int>, greater<int> > minHeap;
-        for(int i = 0; i<nums.size() ; i++){
-            minHeap.push(nums[i]);
+        // 0000000 low..1......mid 01212101201 high .22222222
+        // handle mid-high
+        int l=0;int mid=0;int h=nums.size()-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[mid]==0){
+                swap(nums[l],nums[mid]);
+                mid++;l++;
+            }
+            else if(nums[mid]==1){mid++;}
+            else{
+                swap(nums[mid],nums[h]);
+                h--;
+            }
         }
-
-        while(!minHeap.empty()){
-            ans.push_back(minHeap.top());
-            minHeap.pop();
-        }
-
-        for(int i = 0; i<ans.size(); i++){
-            nums[i] = ans[i];
-        }
+      
     }
 };
