1361a19a471112ac495c8afc688637a4788da2de
Nihit Gupta <nihit9500@gmail.com>
2025-05-19 18:02:21 +0530

    Time: 33 ms (59.17%), Space: 109.1 MB (82.9%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
-#define ll long long
     int shortestSubarray(vector<int>& nums, int k) {
-           int n = nums.size();
+        // brute  prefix sum wala --> o(n^2)
+        //  ll n=a.size(),len=LLONG_MAX;
+        // len=(a[0]>=k) ? 1:len;
+        // for( ll i=1;i<n;i++){
+        //     a[i]+=a[i-1];
+        //     if( a[i]>=k) len=min(len, i+1);
+        // } 
+        // // 2 1 3
+        //  for( ll i=0;i<n;i++){
+        //     for( ll j=i+1;j<n;j++){
+        //         if( a[j]-a[i] >=k) len=min( len, j-i);
+        //     }
+        // }
+        //  return (len!=INT_MAX) ? len:-1;
 
-        // Initialize result to the maximum possible integer value
-        int shortestSubarrayLength = INT_MAX;
-
-        long long cumulativeSum = 0;
-
-        // Min-heap to store cumulative sum and its corresponding index
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                       greater<>>
-            prefixSumHeap;
-
-        // Iterate through the array
-        for (int i = 0; i < n; i++) {
-            // Update cumulative sum
-            cumulativeSum += nums[i];
-
-            // If cumulative sum is already >= k, update shortest length
-            if (cumulativeSum >= k) {
-                shortestSubarrayLength = min(shortestSubarrayLength, i + 1);
+        // same question hai -minimum  siz subarray sum wala
+        // but ime -ve numbers bhi aa gye hai
+        // 84 -37 37  40 95- k=135
+        // prefix sum nikal liyo
+        //    84--->dip 47--->agaibn rise to 84== no value of first 84
+        // usko completely igonre kar sakte hai- kyuki  vo humko bada subarray de sakta hai
+        // /isliye igonre that
+        int n=nums.size();
+        vector<long long>v(n,0);
+        deque<int>dq;
+        int j=0;
+        int ans=INT_MAX;
+        while(j<n){
+            if(j==0){v[j]=nums[j];}
+            else{
+                v[j]=v[j-1]+nums[j];
             }
-
-            // Remove subarrays from heap that can form a valid subarray
-            while (!prefixSumHeap.empty() &&
-                   cumulativeSum - prefixSumHeap.top().first >= k) {
-                // Update shortest subarray length
-                shortestSubarrayLength =
-                    min(shortestSubarrayLength, i - prefixSumHeap.top().second);
-                prefixSumHeap.pop();
-            }
-
-            // Add current cumulative sum and index to heap
-            prefixSumHeap.emplace(cumulativeSum, i);
+            if(v[j]>=k){ans=min(ans,j+1);}
+            while(!dq.empty() and v[j]-v[dq.front()]>=k){
+                ans=min(ans,j-dq.front());
+                dq.pop_front();
+            }      // shrink
+            // monotonic order me daalna hai
+            while(!dq.empty() and v[j]<=v[dq.back()]){dq.pop_back();}
+            dq.push_back(j);
+            j++;
         }
-
-        // Return -1 if no valid subarray found
-        return shortestSubarrayLength == INT_MAX ? -1 : shortestSubarrayLength;
+    if(ans==INT_MAX)return -1;
+    return ans;
     }
 };
