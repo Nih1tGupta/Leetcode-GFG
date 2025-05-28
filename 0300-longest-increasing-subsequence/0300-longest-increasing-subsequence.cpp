0583919324da4aa8e442f5e8f5e49f7c5c5e1033
Nihit Gupta <nihit9500@gmail.com>
2025-05-28 11:55:18 +0530

    Time: 60 ms (61.56%), Space: 14.4 MB (61.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    int t[2501][2501];
-    int n;
-    int solve(vector<int>&nums,int prev,int curr){
-        if(curr==n)return 0;
-        if(prev!=-1 and t[prev][curr]!=-1)return t[prev][curr];
-        int take=0;
-        if(prev==-1|| nums[curr]>nums[prev])take= 1+ solve(nums,curr,curr+1);
-        int nt=solve(nums,prev,curr+1);
-        if(prev!=-1)t[prev][curr]= max(take,nt);
-        return max(take,nt);
+   int solve(int ind , int prev_ind ,vector<int>&nums,int n , vector<vector<int>>&dp){
+        if(ind == n){return 0;}
+        if(dp[ind][prev_ind+1] != -1){return dp[ind][prev_ind + 1];}
+        int len = 0 + solve(ind+1,prev_ind,nums,n,dp);  //not including case
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind])  //including case 
+        {len = max(len , 1 + solve(ind+1,ind,nums,n,dp));}
+        return dp[ind][prev_ind+1] = len;
     }
     int lengthOfLIS(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-        n=nums.size();
-        return solve(nums,-1,0);
+        int n = nums.size();
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // return solve(0,-1,nums,n,dp);
+
+
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int pre=ind-1;pre>=-1;pre--){
+        //          int len = 0 + dp[ind+1][pre+1];  //not including case
+        // if(pre == -1 || nums[ind] > nums[pre])  //including case 
+        // {len = max(len , 1 + dp[ind+1][ind+1]);}
+        //  dp[ind][pre+1] = len;
+        //     }
+        // }
+        // return dp[0][0];
+
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]>nums[prev]){dp[i]= max(dp[i],1+dp[prev]);}
+            }
+             maxi=max(maxi,dp[i]);
+        }
+    return maxi;
     }
 };
