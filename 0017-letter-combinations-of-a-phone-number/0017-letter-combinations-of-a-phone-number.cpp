f75de2036fc96df9b3ad648e62de0d520d96b9c3
Nihit Gupta <nihit9500@gmail.com>
2025-05-30 10:43:11 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,22 @@
 class Solution {
-    private:
-        void combination(string digit,string output,int index,vector<string>&ans,string map[]){
-            if(index>=digit.length()){ans.push_back(output);
-            return ;}
-        int num=digit[index]-'0';
-        string value = map[num];
-        for(int i=0;i<value.length();i++){
-            output.push_back(value[i]);
-              combination(digit,output,index+1,ans,map);
-              output.pop_back();
-                    }
-        }
 public:
+    void solve(string digit,string map[],vector<string>&ans,int idx,string s){
+        // base
+        if(idx>=digit.length()){ans.push_back(s);return;}
+        int num=digit[idx]-'0';
+        string value= map[num];
+        for(int i=0;i<value.length();i++){
+            s.push_back(value[i]);
+            solve(digit,map,ans,idx+1,s);
+            s.pop_back();
+        }
+    }
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-                if(digits.length()==0){return ans;}
-                string output="";
-        int index=0;
         string map[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        combination(digits,output,index,ans,map);
-        return ans;
+        string s="";
+        vector<string>ans;
+        if(digits.length()==0){return ans;}
+        solve(digits,map,ans,0,s);
+    return ans;
     }
 };
