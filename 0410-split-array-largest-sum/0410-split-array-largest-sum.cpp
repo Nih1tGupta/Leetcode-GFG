c9907955854292b2179d5c08df9fa41749140dc2
Nihit Gupta <nihit9500@gmail.com>
2025-06-07 20:57:06 +0530

    Time: 0 ms (100%), Space: 10.3 MB (88.03%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-   int n;
-int dp[1005][56];
-int f(vector<int>&a,int idx,int k){
-    if(idx==n&&k==0) return 0;
-    if(k<0) return INT_MAX;
-    else if(idx==n) return INT_MAX;
-    if(idx>=0&&dp[idx][k]!=-1) return dp[idx][k];
-    int ans=INT_MAX;
-    int sum=0;
-    for(int i=idx;i<n;i++){
-       sum+=a[i];
-        
-        ans=min(ans,max(sum,f(a,i+1,k-1)));
-       
-
+    int possible(vector<int>&nums,int x){
+        int n=nums.size();
+        int st=1;
+        int cap=0;
+        for(int i=0;i<n;i++){
+            if(cap+nums[i]<=x){
+                cap+=nums[i];
+            }
+            else{
+                st++;
+                cap=nums[i];
+            }
+        }
+    return st;
     }
-    return dp[idx][k]=ans;
-}
-    int splitArray(vector<int>& a, int k) {
-      memset(dp,-1,sizeof(dp));
-        n=a.size();
-       
-        return f(a,0,k);
-        
+    int splitArray(vector<int>& nums, int k) {
+        int l=*max_element(nums.begin(),nums.end());
+        int h=0;
+        for(auto it:nums){h+=it;}
+        while(l<=h){
+            int m=(l+h)/2;
+            if(possible(nums,m)<=k){h=m-1;}
+            else{l=m+1;}
+        }
+    return l;
     }
 };
