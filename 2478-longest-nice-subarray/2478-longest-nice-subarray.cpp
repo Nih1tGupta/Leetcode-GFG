83033b1a61d2f21f88ef5051bf7a3370039f6115
Nihit Gupta <nihit9500@gmail.com>
2025-06-08 20:02:06 +0530

    Time: 0 ms (100%), Space: 60.9 MB (63.34%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -14,31 +14,47 @@ public:
         //     r++;
         // }
         // return maxi;
-           bool isNice(vector<int>& nums, int start, int end) {
-        for (int i = start; i <= end; i++) {
-            for (int j = i + 1; j <= end; j++) {
-                if ((nums[i] & nums[j]) != 0) { 
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
+
+
+        // n^4 
+    // bool isNice(vector<int>& nums, int start, int end) {
+    //     for (int i = start; i <= end; i++) {
+    //         for (int j = i + 1; j <= end; j++) {
+    //             if ((nums[i] & nums[j]) != 0) { 
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
     int longestNiceSubarray(vector<int>& nums) {
-        int maxLen = 1;
-        int size = nums.size();
+        // int maxLen = 1;
+        // int size = nums.size();
+        // for (int start = 0; start < size; start++) {
+        //     for (int end = start; end < size; end++) {
+        //         if (isNice(nums, start, end)) {
+        //             maxLen = max(maxLen, end - start + 1);
+        //         } else {
+        //             break; 
+        //         }
+        //     }
+        // }
+        // return maxLen;  n^4
+        //  n^3-> wo jo n2 extra le rahe ho pairwsie check karna ka
+        //  uski jagah mask bana ke solve karlo AND OR WALA-KEEP TRACK OF 1 
 
-        for (int start = 0; start < size; start++) {
-            for (int end = start; end < size; end++) {
-                if (isNice(nums, start, end)) {
-                    maxLen = max(maxLen, end - start + 1);
-                } else {
-                    break; 
-                }
+        // N^2
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            int mask=0;
+            for(int j=i;j<nums.size();j++){
+                if( (mask & nums[j]) !=0){break;}
+                mask |= nums[j];
+                 maxi= max(maxi,j-i+1);
             }
+       
         }
-
-        return maxLen;
+    return maxi;
     }
 };
