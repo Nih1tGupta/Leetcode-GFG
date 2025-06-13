9fc2d6855d7c2eeee3680cb8ebb0800367292d2c
Nihit Gupta <nihit9500@gmail.com>
2025-06-13 20:15:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,29 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-    struct Node *bottom;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-        bottom = NULL;
-    }
-};
-
-void printList(Node *Node) {
-    while (Node != NULL) {
-        printf("%d ", Node->data);
-        Node = Node->bottom;
-    }
-    printf("\n");
-}
-
-
-// } Driver Code Ends
 /* Node structure  used in the program
 
 struct Node{
@@ -43,81 +17,32 @@ struct Node{
 class Solution {
   public:
     // Function which returns the  root of the flattened linked list.
+    
+    Node* merge(Node*l1,Node*l2){
+        Node*dummy= new Node(-1);
+        Node*res=dummy;
+        while(l1!=nullptr and l2!=nullptr){
+            if(l1->data <l2->data){
+                res->bottom=l1;
+                res=l1;
+                l1=l1->bottom;
+            }
+            else {
+                res->bottom=l2;
+                res=l2;
+                l2=l2->bottom;
+            }
+        }
+        if(l1){res->bottom=l1;}
+        if(l2){res->bottom=l2;}
+        // if(dummy->bottom!=nullptr){dummy->bottom->next==nullptr;}
+        return dummy->bottom;
+    }
     Node *flatten(Node *root) {
-       vector<int>v;
-       Node*curr=root;
-    //   O(M*N)
-       while(curr!=nullptr){
-           Node*temp=curr;
-           while(temp!=nullptr){
-               v.push_back(temp->data);
-               temp=temp->bottom;
-           }
-           curr=curr->next;
-       }
-    //   X*LOG(x)
-       sort(v.begin(),v.end());
-       Node*head=new Node(v[0]);
-       Node*curr1=head;
-    //   O(M*N)
-       for(int i=1;i<v.size();i++){
-           curr1->bottom=new Node(v[i]);
-           curr1=curr1->bottom;
-       }
-       return head;
+        // Your code here
+        if(root==nullptr || root->next==nullptr)return root;
+        Node*newHead= flatten(root->next);
+        root= merge(root,newHead);
+        return root;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        vector<int> work;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            work.push_back(number);
-        }
-        n = work.size();
-
-        Node *head = NULL;
-        Node *pre = NULL;
-
-        for (int i = 0; i < n; i++) {
-            int m = work[i] - 1;
-            int data;
-            cin >> data;
-            Node *temp = new Node(data);
-            if (head == NULL) {
-                head = temp;
-                pre = temp;
-            } else {
-                pre->next = temp;
-                pre = temp;
-            }
-
-            Node *preB = temp;
-            for (int j = 0; j < m; j++) {
-                int temp_data;
-                cin >> temp_data;
-                Node *tempB = new Node(temp_data);
-                preB->bottom = tempB;
-                preB = tempB;
-            }
-        }
-
-        Solution ob;
-        Node *root = ob.flatten(head);
-        printList(root);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
