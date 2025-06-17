32adfe8b50fecfb849966be72b1d85a36938a1dd
Nihit Gupta <nihit9500@gmail.com>
2025-06-17 21:45:55 +0530

    Time: 2 ms (13.05%), Space: 8.2 MB (48.08%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-    bool solve(int idx,int ct,string s,vector<vector<int>>&dp){
-        // base
-        if(idx==s.length()){
-            return (ct==0);
-        }
-        if(dp[idx][ct]!=-1)return dp[idx][ct];
-        bool flag=false;
-        if(s[idx]=='*'){
-            flag|= solve(idx+1,ct+1,s,dp);
-            if(ct){flag |=solve(idx+1,ct-1,s,dp);}
-            flag|=solve(idx+1,ct,s,dp);
-        }
-        else if(s[idx]=='('){
-            flag |=solve(idx+1,ct+1,s,dp);
-        }
-        else{
-            if(ct)flag|=solve(idx+1,ct-1,s,dp);
-        }
-        return dp[idx][ct]=flag;
-    }
+    // bool solve(int i,int op,string s,vector<vector<int>>&dp){
+    //     // base
+    //     if(i==s.length()){return (op==0);}
+    //     if(dp[i][op]!=-1)return dp[i][op];
+    //     bool flag=false;
+    //     if(s[i]=='('){
+    //         flag|= solve(i+1,op+1,s,dp);
+    //     }
+    //     else if(s[i]==')'){
+    //         if(op>0)flag|= solve(i+1,op-1,s,dp);
+    //     }
+    //     else{
+    //         flag |=solve(i+1,op+1,s,dp);
+    //         if(op) {flag|=solve(i+1,op-1,s,dp);}
+    //         flag |=solve(i+1,op,s,dp);
+    //     }
+    //     return dp[i][op]=flag;
+    // }
     bool checkValidString(string s) {
-        // int min=0;int max=0;
-        // for(int i=0;i<s.length();i++){
-        //     if(s[i]=='('){min++;max++;}
-        //     else if(s[i]==')'){min--;max--;}
-        //     else{min--;max++;}
-        //     if(min<0)min=0;
-        //     if(max<0)return 0;
-        // }
-        // return min==0;
         int n=s.length();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(0,0,s,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return solve(0,0,s,dp);
+        stack<int>op;
+        stack<int>ast;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){op.push(i);}
+            else if(s[i]=='*'){
+                ast.push(i);
+            }
+            else{
+                if(!op.empty()){op.pop();}
+                else if(!ast.empty()){ast.pop();}
+                else{return false;}
+            }
+        }
+        while(!op.empty() and !ast.empty()){
+            if(op.top()>ast.top()){return false;}
+            op.pop();
+            ast.pop();
+        }
+        return (op.empty());
     }
 };
