65c849697956060dd44eb0261ee49520cb09aa02
Nihit Gupta <nihit9500@gmail.com>
2025-07-04 13:12:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,19 +11,17 @@
  */
 class Solution {
 public:
-    vector<int> solve(TreeNode* root) {
-        if (root == nullptr) {
-            return {0, 0}; // [I, E]
-        }
-        vector<int> left = solve(root->left); // [I, E]
-        vector<int> right = solve(root->right);
-        vector<int> res(2);
-        res[0] = root->val + left[1] + right[1]; // I
-        res[1] = max(left[0], left[1]) + max(right[0], right[1]); // Ex
+    vector<int>solve(TreeNode*root){
+        if(root==nullptr){return {0,0};}
+        vector<int>left=solve(root->left);
+        vector<int>right=solve(root->right);
+        vector<int>res(2);
+        res[0]=root->val + left[1] +right[1];
+        res[1]= max(left[0],left[1]) + max(right[0],left[0]);
         return res;
     }
     int rob(TreeNode* root) {
-         vector<int>y=solve(root);
-        return max(y[0], y[1]);
+        vector<int>v=solve(root);
+        return max(v[0],v[1]);
     }
 };
