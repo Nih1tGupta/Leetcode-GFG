5abe6058271b8229962287854eb1217d1b64fa73
Nihit Gupta <nihit9500@gmail.com>
2025-07-04 17:38:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
      int sum(vector<int>&arr, vector<vector<int>>&dp,int target,int index){
@@ -34,35 +28,3 @@ class Solution {
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int target;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> target;
-        cin.ignore(); // Ignore newline character after target input
-
-        Solution solution;
-        cout << solution.perfectSum(arr, target);
-        cout << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
