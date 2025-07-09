8f02c907e891cb79c5f2ceee87241051570879ce
Nihit Gupta <nihit9500@gmail.com>
2025-07-09 16:52:51 +0530

    Time: 4 ms (50.72%), Space: 27.4 MB (60.41%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,26 +11,23 @@
  */
 class Solution {
 public:
-int help(vector<int>& inorder, int n, int element)
-    {
-        for(int i=0; i<n; i++)
-            if(inorder[i]==element)
-                return i;
-        return -1;
+ TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        map<int,int> mp;
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]] = i;
+        }
+        TreeNode* root = build(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1,mp);
+        return root;
     }
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int n,int &preIndex,int startIn,int endIn){
-        if(preIndex<0||startIn>endIn)return nullptr;
-        int ele=preorder[preIndex--];
-        TreeNode*root= new TreeNode(ele);
-        int pos= help(inorder,n,ele);
-        root->right=solve(preorder,inorder,n,preIndex,pos+1,endIn);
-         root->left=solve(preorder,inorder,n,preIndex,startIn,pos-1);
-         return root;
-    }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n=postorder.size();
-        int postIndex=n-1;int startIn=0;int endIn=n-1;
-        return solve(postorder,inorder,n,postIndex,startIn,endIn);
-    
+    TreeNode* build(vector<int>& postorder,int poststart, int postend, vector<int>& inorder, int instart, int inend,map<int,int>& mp){
+        if(poststart > postend || instart > inend){
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[postend]);
+        int inroot = mp[root->val];
+        int numsleft = inroot - instart;
+        root->left = build(postorder,poststart,poststart + numsleft -1,inorder,instart,inroot-1,mp);
+        root->right = build(postorder,poststart + numsleft,postend-1,inorder,inroot+1,inend,mp);
+        return root;
     }
 };
