d65e8bbeca6049a79678f3dabbd6423d833c1831
Nihit Gupta <nihit9500@gmail.com>
2025-07-09 16:35:37 +0530

    Time: 3 ms (70.48%), Space: 123.3 MB (74.52%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int maxFreeTime(int et, int k, vector<int>& s, vector<int>& e) {
-        int maxi=0;
-        int n=s.size();
-        vector<int>gap(n+1);
-        gap[0]=s[0]; gap[n]=et-e[n-1];
-        for(int i=1;i<e.size();i++){
-            gap[i]=(s[i]-e[i-1]);
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+            int n = startTime.size();
+        vector<int> gaps;
+        gaps.reserve(n + 1);
+        gaps.push_back(startTime[0]);
+        for (int i = 1; i < n; ++i) {
+            gaps.push_back(startTime[i] - endTime[i - 1]);
         }
-        int w=0;
-        for(int i=0;i<=k;i++){
-            w+=gap[i];
+        gaps.push_back(eventTime - endTime[n - 1]);
+        int sz = min(k + 1, int(gaps.size()));
+        long long curr = 0, best = 0;
+        for (int i = 0; i < sz; ++i) curr += gaps[i];
+        best = curr;
+        for (int i = sz; i < gaps.size(); ++i) {
+            curr += gaps[i] - gaps[i - sz];
+            if (curr > best) best = curr;
         }
-        maxi=w;
-        for(int i=k+1;i<=n;i++){
-            w+=gap[i]-gap[i-(k+1)];
-            maxi=max(maxi,w);
-        }
-        return maxi;
+        return int(best);
     }
 };
