7aa27f898c942a954c825bf5dbaabca816694679
Nihit Gupta <nihit9500@gmail.com>
2025-07-24 01:20:16 +0530

    Time: 17 ms (18.8%), Space: 17.4 MB (68.44%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,43 +12,36 @@ public:
     // bool helper(TreeNode*root,vector<TreeNode*>&v,TreeNode*x){
     //     if(root==nullptr)return false;
     //     v.push_back(root);
-    //     if(root==x)return 1;
-    //     if(helper(root->left,v,x)|| helper(root->right,v,x)){return 1;}
+    //     if(root==x)return true;
+    //     if(helper(root->left,v,x) || helper(root->right,v,x)){return 1;}
     //     v.pop_back();
     //     return false;
     // }
     // vector<TreeNode*>help(TreeNode*root,TreeNode*x){
     //     vector<TreeNode*>v;
     //     if(root==nullptr)return v;
-    //      helper(root,v,x);
-    //      return v;
+    //     helper(root,v,x);
+    //     return v;
     // }
     // TreeNode*solve(vector<TreeNode*>a,vector<TreeNode*>b){
-    //     int l=min(a.size(),b.size());
-    //     TreeNode*LCA=nullptr;
+    //     int l= min(a.size(),b.size());
+    //     TreeNode*ans=nullptr;
     //     for(int i=0;i<l;i++){
-    //         if(a[i]==b[i])LCA=a[i];
+    //         if(a[i]==b[i]){ans=a[i];}
     //     }
-    //     return LCA;
+    //     return ans;
     // }
-
-    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
-        // basecase
-        if(root==nullptr||root==p||root==q){return root;}
-        TreeNode*l=solve(root->left,p,q);
-        TreeNode*r=solve(root->right,p,q);
-        if(l!=nullptr and r!=nullptr){return root;}
-        else{
-            return (l==nullptr)?r:l;
-        }
-        return nullptr;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // vector<TreeNode*>path_p=help(root,p);
-        // vector<TreeNode*>path_q=help(root,q);
-        // TreeNode*ans= solve(path_p,path_q);
-        // return ans;
-        TreeNode*ans=solve(root,p,q);
-        return ans;
+        // if(root==nullptr || p==nullptr || q==nullptr)return root;
+        // vector<TreeNode*>p1=help(root,p);
+        // vector<TreeNode*>p2=help(root,q);
+        // TreeNode*x=solve(p1,p2);
+        // return x;
+         if(root==nullptr||root==p||root==q)return root;
+        TreeNode*left=lowestCommonAncestor(root->left,p,q);
+        TreeNode*right=lowestCommonAncestor(root->right,p,q);
+        if(left==nullptr)return right;
+        else if(right==nullptr)return left;
+         return root;
     }
 };
