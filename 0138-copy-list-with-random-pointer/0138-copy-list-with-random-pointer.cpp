17724979fbf01451e7c78b0c9d983605e849d504
Nihit Gupta <nihit9500@gmail.com>
2025-07-24 17:37:34 +0530

    Time: 3 ms (91.79%), Space: 15.1 MB (45.6%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,36 +17,59 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head==nullptr)return nullptr;
-        Node*temp=head;
-        while(temp){
-            Node*copy=new Node(temp->val);
-            copy->next=temp->next;
-            temp->next=copy;
-            temp=temp->next->next;
-        }
+    //     if(head==nullptr)return nullptr;
+    //     Node*temp=head;
+    //     while(temp){
+    //         Node*copy=new Node(temp->val);
+    //         copy->next=temp->next;
+    //         temp->next=copy;
+    //         temp=temp->next->next;
+    //     }
 
-        temp=head;
-        while(temp){
-            Node*copy= temp->next;
-            if(temp->random){
-                copy->random= temp->random->next;
-            }
-            else{
-                copy->random=nullptr;
-            }
-            temp=temp->next->next;
-        }
+    //     temp=head;
+    //     while(temp){
+    //         Node*copy= temp->next;
+    //         if(temp->random){
+    //             copy->random= temp->random->next;
+    //         }
+    //         else{
+    //             copy->random=nullptr;
+    //         }
+    //         temp=temp->next->next;
+    //     }
 
-        Node*dummy=new Node(-1);
-        Node*res=dummy;
-        temp=head;
-        while(temp){
-            res->next=temp->next;
-            temp->next=temp->next->next;
-            res=res->next;
-            temp=temp->next;
+    //     Node*dummy=new Node(-1);
+    //     Node*res=dummy;
+    //     temp=head;
+    //     while(temp){
+    //         res->next=temp->next;
+    //         temp->next=temp->next->next;
+    //         res=res->next;
+    //         temp=temp->next;
+    //     }
+    //     return dummy->next;
+        Node* clone = head;
+        unordered_map<Node*, Node*> map;
+        while(clone) {
+            Node* curr = new Node(clone->val);
+            map[clone] = curr;
+            
+            clone = clone->next;
         }
-        return dummy->next;
+        
+        Node* res = new Node(-1);
+        Node* ret = head;
+        while(head) {
+            res = map[head];
+            
+            res->random = map[head->random];
+            res->next = map[head->next];
+            
+            res = res->next;
+            head = head->next;
+        }
+        
+        
+        return map[ret];
     }
 };
