a953a0c5ec4507af6fe077f7ea22f39498ac2e15
Nihit Gupta <nihit9500@gmail.com>
2025-08-02 10:49:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -1,91 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
 class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
         // Code here
-        
-        // BRUTE--->O(N^2)*X
-        
-        
-        
-        // 2 POINTER -FAILED
-        
-        
-        // int i=0;int j=0;
-        // int gr=0;int le=0; int maxi=0;
-        // while(j<arr.size()){
-        //     while(gs<ls){
-                
-        //     }
-        //     if(arr[j]>k){
-        //         gr++;
-        //         if(gr>ls){maxi=max(maxi,j-i+1);}
-        //     }
-        //     else{ls++;}
-        //     j++;
-        // }
-          int n=arr.size();
-        vector<int>diff(n);
-        for(int i=0;i<n;i++){
-            if(arr[i]>k){
-                diff[i]=1;
-            }
-            else{
-                diff[i]=-1;
-            }
-        }
+        // special case
+        // if pehle kabhi current se bhi choota mila toh 
+        // ye oth pakka hai ki +1karke hi aay hoga- usko consider karo
         unordered_map<int,int>mp;
-        int prefix=0;
-        int maxlen=0;
-        for(int i=0;i<n;i++){
-            prefix+=diff[i];
-            if(prefix>0){
-                maxlen=i+1;
+        int maxi=0;
+        mp[0]=-1;int sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=(arr[i]>k)?1:-1;
+            if(sum>0)maxi=i+1;
+            if(mp.find(sum-1)!=mp.end()){
+                maxi=max(maxi, i-mp[sum-1]);
             }
             else{
-                if(mp.find(prefix-1)!=mp.end()){
-                    maxlen=max(maxlen,i-mp[prefix-1]);
-                }
-            }
-            if(mp.find(prefix)==mp.end()){
-                mp[prefix]=i;
+                if(mp.find(sum)==mp.end()){mp[sum]=i;}
             }
         }
-        return maxlen;
+        return maxi;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        int k = stoi(input);
-
-        Solution ob;
-        cout << ob.longestSubarray(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
