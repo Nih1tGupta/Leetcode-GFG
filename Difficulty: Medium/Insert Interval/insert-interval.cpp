1b46c78bc7920d23bc9209a735f1a97ad87c8d04
Nihit Gupta <nihit9500@gmail.com>
2025-08-15 23:11:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.cpp
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -1,15 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,vector<int> &newInterval) {
-        vector<vector<int>>v;
+    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
+                                       vector<int> &newInterval) {
+        // code here
+         vector<vector<int>>v;
         int n=intervals.size();int i=0;
         while(i<n && newInterval[0]>intervals[i][1]){
             v.push_back(intervals[i]);
@@ -22,38 +16,7 @@ class Solution {
        }
        v.push_back(newInterval);
        while(i<n and newInterval[1]<intervals[i][0]){v.push_back(intervals[i]);i++;}
-        return v;
+        return  v;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<vector<int>> intervals(N, vector<int>(2));
-        for (int i = 0; i < N; i++) {
-            cin >> intervals[i][0] >> intervals[i][1];
-        }
-        vector<int> newInterval(2);
-        cin >> newInterval[0] >> newInterval[1];
-
-        Solution obj;
-        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
-        cout << "[";
-        for (int i = 0; i < ans.size(); i++) {
-            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
-            if (i != (ans.size() - 1))
-                cout << ",";
-        }
-        cout << "]" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
