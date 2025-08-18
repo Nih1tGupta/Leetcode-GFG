964858bf7d2504d6d7e850c1a8fd2973300260e3
Nihit Gupta <nihit9500@gmail.com>
2025-08-18 15:06:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,55 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find hIndex
+    
+        // code here
+         bool isPossible(vector<int>& citations, int mid){
+      int n = citations.size();
+       int count = 0  ;
+      for(int i = 0 ; i  < n; i++){
+          if(citations[i] >= mid){
+              count++;
+          }
+      }
+      return count>=mid; 
+  }
+  
     int hIndex(vector<int>& citations) {
         // code here
-        sort(citations.begin(), citations.end(), greater<int>());
-        int n=citations.size();
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-         if(citations[i]>=i+1)ans++;
-         else{break;}
+        int n = citations.size();
+        
+        int l = 0 ; 
+        int r = n ; 
+        
+        int result = 0 ; 
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            
+            if(isPossible(citations, mid)){
+               result = mid ; 
+               l = mid + 1; 
+            }
+            else r = mid -1; 
         }
-        return ans;
+        return result;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int test_cases;
-    cin >> test_cases;
-    cin.ignore();
-    while (test_cases--) {
-        string input;
-        vector<int> arr;
-
-        // Read the array from input line
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Solution instance to invoke the function
-        Solution ob;
-        int result = ob.hIndex(arr);
-
-        cout << result << endl;
-
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
