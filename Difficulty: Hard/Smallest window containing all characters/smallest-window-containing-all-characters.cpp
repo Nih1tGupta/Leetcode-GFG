6ee6ccd964b398399f7f5a96bb342677074b1ff2
Nihit Gupta <nihit9500@gmail.com>
2025-08-29 09:24:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    string smallestWindow(string &s, string &t) {
+        // code here
+         unordered_map<char,int>mp;
+        for(char ch:t){mp[ch]++;}
+        int n=s.length();
+        if(t.length()>n)return "";
+        int mini=INT_MAX;
+        int ct=t.length();
+        int i=0;int j=0;int idx=0;
+        while(j<n){
+            char ch=s[j];
+            if(mp[ch]>0){ct--;}
+            mp[ch]--;
+            while(ct==0){
+                if(j-i+1<mini){mini=j-i+1;idx=i;}
+                mp[s[i]]++;
+                if(mp[s[i]]>0){ct++;}
+                i++;
+            }
+            j++;
+        }
+        return mini==INT_MAX?"":s.substr(idx,mini);
+    }
+};
