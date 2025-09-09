60c3ab95920a4d3daaab4ec71678e99bcba2e374
Nihit Gupta <nihit9500@gmail.com>
2025-09-09 15:52:06 +0530

    Time: 5 ms (20.12%), Space: 8.6 MB (20.3%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10)return 0;
+             rev=rev*10+rem;
+            x/=10;
+        }
+        return rev;
+    }
+};
