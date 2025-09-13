2f59b09d785415fb8cafa0586f11336a15576d4e
Nihit Gupta <nihit9500@gmail.com>
2025-09-13 23:15:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Cost to cut a board into squares/minimum-cost-to-cut-a-board-into-squares.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    int minCost(int n, int m, vector<int>& x, vector<int>& y) {
+        //  code here
+        // greedy->    sort---> cut for large- (keep freq of cnt)
+          sort(x.rbegin(), x.rend());
+        sort(y.rbegin(), y.rend());
+
+        int i = 0, j = 0;
+        int horizontal_segments = 1, vertical_segments = 1;
+        int total_cost = 0;
+
+        // Greedy selection
+        while (i < x.size() && j < y.size()) {
+            if (x[i] >= y[j]) {
+                total_cost += x[i] * horizontal_segments;
+                vertical_segments++;
+                i++;
+            } else {
+                total_cost += y[j] * vertical_segments;
+                horizontal_segments++;
+                j++;
+            }
+        }
+
+        // Remaining vertical cuts
+        while (i < x.size()) {
+            total_cost += x[i] * horizontal_segments;
+            vertical_segments++;
+            i++;
+        }
+
+        // Remaining horizontal cuts
+        while (j < y.size()) {
+            total_cost += y[j] * vertical_segments;
+            horizontal_segments++;
+            j++;
+        }
+
+        return total_cost;
+    }
+};
