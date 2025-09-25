de69a3fc1adfc8d7e679415226fc34dd7bc460f7
Nihit Gupta <nihit9500@gmail.com>
2025-09-25 18:02:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    
+    vector<string> generateBinary(int n) {
+        // code here
+        vector<string>v;
+        queue<string>q;
+        q.push("1");
+        for(int i=1;i<=n;i++){
+            string curr=q.front(); q.pop();
+            v.push_back(curr);
+            q.push(curr+"0");
+            q.push(curr+"1");
+        }
+        return v;
+    }
+};
