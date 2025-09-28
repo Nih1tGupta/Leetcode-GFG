19bf9be5650b27bebe2de8b2e43a8610e94e4347
Nihit Gupta <nihit9500@gmail.com>
2025-09-28 11:01:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int num=x;
+        int x1=0;
+        while(x>0){
+            int rem=x%10;
+            x1=x1*10+rem;
+            x/=10;
+        }
+        return (x1==num);
+    }
+};
