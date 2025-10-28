7381b5750a7411801a481204d92774717f8b0528
Nihit Gupta <nihit9500@gmail.com>
2025-10-28 13:29:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,16 +1,8 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to find distance of nearest 1 in the grid for each cell.
-      
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
-        // Code here
-        int n=grid.size();int m=grid[0].size();
+        // code here
+          int n=grid.size();int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         vector<vector<int>>dis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
@@ -43,29 +35,3 @@ class Solution {
         return dis;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> grid(n, vector<int>(m, -1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> grid[i][j];
-            }
-        }
-        Solution obj;
-        vector<vector<int>> ans = obj.nearest(grid);
-        for (auto i : ans) {
-            for (auto j : i) {
-                cout << j << " ";
-            }
-            cout << "\n";
-        }
-    }
-    return 0;
-}
-// } Driver Code Ends
