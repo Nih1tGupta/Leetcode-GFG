c8fbf5638f39f8c7f32df2f4b4ee6138c286dd10
Nihit Gupta <nihit9500@gmail.com>
2025-11-04 15:08:13 +0530

    Time: 16 ms (50.48%), Space: 37 MB (34.5%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    class comp{
-        bool operator()(pair<int,int>p1,pair<int,int>p2){
-            if(p1.second==p2.second)return p2.first >p1.first;
-            return p2.second >p1.second;
-        }
-    };
+    // class comp{
+    //     bool operator()(pair<int,int>p1,pair<int,int>p2){
+    //         if(p1.second==p2.second)return p2.first >p1.first;
+    //         return p2.second >p1.second;
+    //     }
+    // };
     vector<int> findXSum(vector<int>& nums, int k, int x) {
-        vector<int>v;
-        int n=nums.size();
-        for(int i=0;i<=n-k;i++){
-            map<int,int>m;
-            for(int j=i;j<i+k;i++){m[nums[j]]++;}
-            priority_queue<pair<int,int>,vector<pair<int,int>>,comp>pq;
-            for(auto it:m){pq.push({it.first,it.second});}
-            int sum=0;
-            for(int i=0;i<x && !pq.empty(); i++){
-                sum+=(pq.top().first)*pq.top().second;
-                pq.pop();
+        int res=0;int n=nums.size();
+        vector<int>v(n-k+1);
+        for(int i=0;i<n-k+1;i++){
+            map<int,int>mp;
+            for(int j=i;j<i+k;j++){
+                mp[nums[j]]++;
             }
-            v.push_back(sum);
+            vector<pair<int,int>>p;
+            for(auto it:mp){
+            p.push_back({it.second,it.first});
+            }
+            sort(p.begin(),p.end(),greater<pair<int,int>>());
+            for(int j=0;j<p.size();j++){
+                if(j<x){
+                v[i]+=p[j].first*p[j].second;
+                }
+            }
         }
-    return v;
+        return v;
     }
 };
