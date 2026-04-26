dbb36bcfb1cd17712eb84921a38806443a24105d
Nihit Gupta <nihit9500@gmail.com>
2026-04-26 13:09:15 +0530

    Time: 25 ms (67.59%), Space: 66.7 MB (72.95%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
-     int m, n;
-    vector<vector<bool>> visited;
-    
-    bool dfs(int x, int y, int px, int py, vector<vector<char>>& grid) {
-        visited[x][y] = true;
-        
-        int dirs[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        
-        for (auto &d : dirs) {
-            int nx = x + d[0];
-            int ny = y + d[1];
-            
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n)
-                continue;
-            
-            if (grid[nx][ny] != grid[x][y])
-                continue;
-            
-            // Skip the parent cell
-            if (nx == px && ny == py)
-                continue;
-            
-            // If already visited → cycle found
-            if (visited[nx][ny])
-                return true;
-            
-            if (dfs(nx, ny, x, y, grid))
-                return true;
+    bool dfs(int i,int j,int pi,int pj,vector<vector<bool>>&vis,vector<vector<char>>&grid,int dr[], int dc[], int m,int n){
+        vis[i][j]=1;
+        for(int k=0;k<4;k++){
+            int ni = i +dr[k]; int nj = j +dc[k];
+            if(ni <0 || ni >=m || nj <0 || nj >=n){continue;}
+            if(grid[ni][nj]!= grid[i][j])continue;
+            if(ni ==pi && nj ==pj){continue;}
+            if(vis[ni][nj])return true;
+            if(dfs(ni,nj,i,j,vis,grid,dr,dc,m,n))return true;
         }
-        
         return false;
     }
-    
     bool containsCycle(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        
-        visited.assign(m, vector<bool>(n, false));
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    if (dfs(i, j, -1, -1, grid))
-                        return true;
+        int m = grid.size(); int n = grid[0].size();
+        vector<vector<bool>>vis(m,vector<bool>(n,0));
+        int dr[4]={-1,0,1,0};
+        int dc[4]= {0,1,0,-1};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0){
+                    if(dfs(i,j,-1,-1,vis,grid,dr,dc,m,n))return true;
                 }
             }
         }
-        
         return false;
     }
 };
